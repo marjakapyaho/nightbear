1c93a5d45f26ba659848b8bafcb346477c0d9d8f
Marja Käpyaho <marja.kapyaho@futurice.com>
2015-11-29 16:01:21 +0200

    small syntax changes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { nightscoutUploaderPost } from './api';
+import * as api from './api';
 
 const app = express().use(bodyParser.json());
 
-app.post('/api/v1/entries', (req, res) => {
-    nightscoutUploaderPost(req.body).then(
+app.post('/api/v1/entries', function(req, res) {
+    api.nightscoutUploaderPost(req.body).then(
         data => res.status(200).send(data || null),
         err => res.status(500).send({ error: err && err.message || true })
     );
 });
 
-const server = app.listen(3000, () => {
+const server = app.listen(3000, function() {
     console.log('nightbear server listening on port %s', server.address().port);
 });
