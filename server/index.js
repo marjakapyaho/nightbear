b144bd84670d06da06ffec1d2f742e6ab75e21cb
Jarno Rantanen <jarno@jrw.fi>
2016-03-03 00:52:31 +0200

    Use in-memory DB when DB_URL not provided.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import PouchDB from 'pouchdb';
 import Pushover from 'pushover-notifications';
 import createAppInstance from './app/';
+import MemDOWN from 'memdown';
 import Logger from './utils/Logger';
 
 const app = createAppInstance({
     logger: new Logger(true, process.env.PAPERTRAIL_URL),
     currentTime: Date.now,
-    pouchDB: new PouchDB(process.env.DB_URL, { skip_setup: true }),
+    pouchDB: process.env.DB_URL
+        ? new PouchDB(process.env.DB_URL, { skip_setup: true }) // connect to actual DB
+        : new PouchDB('local-dev-db', { db: MemDOWN }), // use a throw-away in-memory DB
     pushover: new Pushover({
         user: process.env.PUSHOVER_USER,
         token: process.env.PUSHOVER_TOKEN
