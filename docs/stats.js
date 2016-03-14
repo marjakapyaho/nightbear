21052c2ed92ddc823c21fe56bcc0a9b8960e2aba
Jarno Rantanen <jarno@jrw.fi>
2016-03-14 13:26:35 +0200

    Add some other interesting stats.

--- docs/stats.js
+++ docs/stats.js
@@ -1,14 +1,22 @@
 // This file contains some PoC code for producing more interesting stats out of Cloudant
 
 // Setup
+var PouchDB = require('../server/node_modules/pouchdb');
 var db = new PouchDB(process.env.DB_URL);
 var out = x => console.log(x);
 
-// Ensure the view exists
+// Ensure the views exist
 db.get('_design/global_stats')
     .catch(() => ({ _id: '_design/global_stats' }))
     .then(doc => {
         doc.views = {
+            per_type: {
+                map: function (doc) {
+                    var match = doc._id.match(/(.*)\/(.*)T/);
+                    if (match) emit([ match[1] ]); // e.g. [ "calibrations" ]
+                }.toString(),
+                reduce: '_count'
+            },
             per_date_and_type: {
                 map: function (doc) {
                     var match = doc._id.match(/(.*)\/(.*)T/);
@@ -21,7 +29,17 @@ db.get('_design/global_stats')
     })
     .then(out, out);
 
-// Query the view
+// How many docs per type
+db.query('global_stats/per_type', { group: true })
+    .then(res => res.rows.map(r => r.key.concat(r.value)))
+    .then(out, out);
+
+// How many docs per day and type
 db.query('global_stats/per_date_and_type', { group: true })
+    .then(res => res.rows.map(r => r.key.concat(r.value)))
+    .then(out, out);
+
+// How many docs per day
+db.query('global_stats/per_date_and_type', { group: true, group_level: 1 })
     .then(res => res.rows.map(r => r.key.concat(r.value)))
     .then(out, out);
