2fec28417c9a7cc2e0a1ba58701a599cd7fe7b76
Jarno Rantanen <jarno@jrw.fi>
2016-03-26 18:27:24 +0200

    Add test case for outdated data analysis.

--- server/test/analyser.spec.js
+++ server/test/analyser.spec.js
@@ -6,34 +6,54 @@ describe('analyser', () => {
 
     const { assertEqual } = testUtils;
 
-    const TIMESTAMP_BASE = 1459000000000; // Sat Mar 26 2016 15:46:40 GMT+0200 (EET), arbitrary but nicely aligned
-    const TIMESTAMP_TICK = 1000 * 60 * 5; // 5 min == 300000 ms
+    const NIGHTTIME = 1458960000000; // Sat Mar 26 2016 04:40:00 GMT+0200 (EET), arbitrary but nicely aligned
+    const DAYTIME = 1459000000000; // Sat Mar 26 2016 15:46:40 GMT+0200 (EET), ^ ditto
+    const TIMELINE_TICK = 1000 * 60 * 5; // 5 min == 300000 ms
 
-    function assertTimelineAnalysis(...expectedTimeline) {
+    const situationObjectToArray = x => _.compact(_.map(x, (val, key) => val ? key : null)); // e.g. { high: true, low: false } => [ 'high' ]
+
+    function assertTimelineAnalysis(startTimestamp, ...expectedTimeline) {
         const actualTimeline = expectedTimeline.map((timelineEntry, i) => {
-            const currentTimestamp = TIMESTAMP_BASE + i * TIMESTAMP_TICK;
-            const analysisResult = analyser.analyseTimelineSnapshot({
+            const currentTimestamp = startTimestamp + i * TIMELINE_TICK;
+            const snapshot = {
                 currentTimestamp,
                 activeProfile: analyser.getActiveProfile(currentTimestamp),
                 latestEntries: expectedTimeline
                     .slice(0, i + 1) // all entries thus far
                     .map(([ { nb_glucose_value, direction } ], j) => ({
-                        date: TIMESTAMP_BASE + j * TIMESTAMP_TICK,
+                        date: startTimestamp + j * TIMELINE_TICK,
                         direction,
                         nb_glucose_value
                     })),
                 latestTreatments: [],
                 latestDeviceStatus: [],
                 latestAlarms: []
-            });
-            const actualSituations = _.compact(_.map(analysisResult, (val, key) => val ? key : null)); // e.g. { high: true, low: false } => [ 'high' ]
+            };
+            const actualSituations = situationObjectToArray(analyser.analyseTimelineSnapshot(snapshot));
             return timelineEntry.slice(0, 1).concat(actualSituations)
         });
         assertEqual(actualTimeline, expectedTimeline);
     }
 
+    it('detects outdated data', () => {
+        assertEqual(
+            situationObjectToArray(
+                analyser.analyseTimelineSnapshot({
+                    currentTimestamp: DAYTIME,
+                    activeProfile: analyser.getActiveProfile(DAYTIME),
+                    latestEntries: [],
+                    latestTreatments: [],
+                    latestDeviceStatus: [],
+                    latestAlarms: []
+                })
+            ),
+            [ analyser.STATUS_OUTDATED ]
+        );
+    });
+
     it('detects highs', () => {
         assertTimelineAnalysis(
+            DAYTIME,
             [ { nb_glucose_value:  8, direction: 'SingleUp'   } ],
             [ { nb_glucose_value: 10, direction: 'SingleUp'   } ],
             [ { nb_glucose_value: 16, direction: 'SingleUp'   }, analyser.STATUS_HIGH ],
@@ -42,5 +62,5 @@ describe('analyser', () => {
             [ { nb_glucose_value:  8, direction: 'SingleDown' } ],
         );
     });
-    
+
 });
