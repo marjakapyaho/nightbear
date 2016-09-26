19d5042ec19b7b822a9bdf888564f1db595fd615
Jarno Rantanen <jarno@jrw.fi>
2016-09-26 23:25:04 +0300

    Add env handling.

--- backend/handler.js
+++ backend/handler.js
@@ -1,8 +1,13 @@
 'use strict';
 
+require('dotenv').config();
+
 // Your first function handler
 module.exports.hello = (event, context, cb) => {
-  cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  cb(null, {
+    message: 'Hello from Nightbear API',
+    dbUrl: process.env.NIGHTBEAR_API_DB_URL
+  });
 };
 
 // You can add more handlers here, and reference them in serverless.yml
