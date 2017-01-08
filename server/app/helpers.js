5a7ea14db3d3ef6983bea527c58da93c13a751e7
Marja Käpyaho <marja.kapyaho@futurice.com>
2017-01-08 19:29:54 +0200

    new raw calculation algorithm

--- server/app/helpers.js
+++ server/app/helpers.js
@@ -4,6 +4,8 @@ export const NOISE_LEVEL_LIMIT = 4;
 export const DIRECTION_NOT_COMPUTABLE = 'NOT COMPUTABLE'; // when direction not computable, use raw sensor data instead
 export const DEFAULT_TREATMENT_TYPE = 'Meal Bolus'; // this is somewhat arbitrary, but "Meal Bolus" is the most applicable of the types available in Nightscout
 
+export let rawCalculationRatio = 1; // default to 1 which doesn't do anything, TODO: make this resettable in tests
+
 // Updates the given entry by interpreting RAW data where necessary, and converting units
 export function setActualGlucose(entry, latestCalibration) {
     let valueToUse = entry.sgv;
@@ -29,7 +31,22 @@ export function isoTimestamp(timeInMs) {
 }
 
 export function calculateRaw(dataPoint, calData) {
-    return calData.scale * (dataPoint.unfiltered - calData.intercept) / calData.slope;
+    let raw = 0;
+    const cleaned = cleanValues(dataPoint, calData);
+
+    if (cleaned.slope === 0 || cleaned.unfiltered === 0 || cleaned.scale === 0) {
+        raw = 0;
+    }
+    else if (cleaned.filtered === 0 || (dataPoint.sgv && dataPoint.sgv < 40)) {
+        raw = cleaned.scale * (cleaned.unfiltered - cleaned.intercept) / cleaned.slope;
+    }
+    else {
+        if (dataPoint.sgv) {
+            rawCalculationRatio = cleaned.scale * (cleaned.filtered - cleaned.intercept) / cleaned.slope / dataPoint.sgv;
+        }
+        raw = cleaned.scale * (cleaned.unfiltered - cleaned.intercept) / cleaned.slope / rawCalculationRatio;
+    }
+    return raw;
 }
 
 // Converts blood glucose values from mg/dL (used by Dexcom) to mmol/L (used in Europe), and rounds to 1 decimal
@@ -66,4 +83,14 @@ export function convertRawTransmitterData(app, entry, latestCalibration) {
 
 export function convertCurrentTimeForParakeet(app, timeEntry) {
     return parseInt(parseInt(app.currentTime(), 10) - (parseInt(timeEntry, 10) / 1000) + "000", 10)
+}
+
+function cleanValues (entry, cal) {
+    return {
+        unfiltered: parseInt(entry.unfiltered) || 0,
+        filtered: parseInt(entry.filtered) || 0,
+        scale: parseFloat(cal.scale) || 0,
+        intercept: parseFloat(cal.intercept) || 0,
+        slope: parseFloat(cal.slope) || 0
+    };
 }
