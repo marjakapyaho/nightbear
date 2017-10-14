c85f960249d1320885fdcc68db73b97cd37bee67
Jarno Rantanen <jarno@jrw.fi>
2017-10-14 18:13:58 +0300

    Add first functional API handler.

--- server/utils/express.ts
+++ server/utils/express.ts
@@ -1,16 +1,40 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
+import { RequestHandler, Context } from './api';
+import { getUuid } from './uuid';
+import { bindLoggingContext, getContextName, handlerWithLogging } from './logging';
 
-export function startExpressServer(): Promise<number> {
+export type HttpMethod = 'get' | 'post';
+export type RequestHandlerTuple = [ HttpMethod, string, RequestHandler ];
+
+export function startExpressServer(context: Context, ...handlers: RequestHandlerTuple[]): Promise<number> {
   return new Promise((resolve, reject) => {
     const app = express();
     app.use(cors());
     app.use(bodyParser.json());
-    app.get('/', (_, res) => {
-      res.status(200).json({ hello: 'world' });
+    handlers.forEach(([ method, path, handler ]) => {
+      app[method](path, (req, res) => {
+        const requestId = req.get('X-Request-ID') || getUuid(); // use Heroku-style req-ID where available, but fall back to our own
+        Promise.resolve({
+          requestId,
+          requestMethod: req.method,
+          requestPath: req.path,
+          requestParams: req.query,
+          requestHeaders: req.headers,
+          requestBody: req.body,
+        })
+          .then(request => {
+            const log = bindLoggingContext(context.log, getContextName('request', requestId));
+            return handlerWithLogging(handler, log)(request, context);
+          })
+          .then(
+            response => res.status(response.responseStatus).json(response.responseBody),
+            () => res.status(500).json({ errorMessage: `Nightbear Server Error (see logs for requestId ${requestId})` }),
+          );
+      });
     });
-    const server = app.listen(3000, () => {
+    const server = app.listen(context.httpPort, () => {
       resolve(server.address().port);
     });
     server.on('error', err => reject(err));
