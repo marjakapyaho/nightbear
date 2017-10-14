2a064623a06fa4e463e119331682e48e797b3a6e
Jarno Rantanen <jarno@jrw.fi>
2017-10-14 18:13:58 +0300

    Add first functional API handler.

---
+++ server/utils/api.ts
@@ -0,0 +1,40 @@
+import { Logger, createConsoleLogger } from './logging';
+
+export function createNodeContext(): Context {
+  return {
+    httpPort: 3000,
+    timestamp: Date.now,
+    log: createConsoleLogger(),
+  };
+}
+
+export interface Request {
+  requestId: string;
+  requestMethod: string;
+  requestPath: string;
+  requestParams: object;
+  requestHeaders: object;
+  requestBody: object;
+}
+
+export interface ResponsePayload {
+  responseStatus: number;
+  responseBody: object;
+}
+
+export type Response = Promise<ResponsePayload>;
+
+export interface Context {
+  httpPort: number;
+  timestamp: () => number;
+  log: Logger;
+}
+
+export type RequestHandler = (request: Request, context: Context) => Response;
+
+export function createResponse(responseBody: object): Response {
+  return Promise.resolve({
+    responseStatus: 200,
+    responseBody,
+  });
+}
