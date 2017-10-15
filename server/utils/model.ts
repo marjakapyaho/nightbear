afff0c74d6ce8a7f261cf4d93198d3e910f0c2db
Jarno Rantanen <jarno@jrw.fi>
2017-10-15 22:07:16 +0300

    Make all Model properties readonly.

--- server/utils/model.ts
+++ server/utils/model.ts
@@ -13,13 +13,13 @@ export type Model
 
 export interface Sensor {
   // Model:
-  modelType: 'Sensor';
-  modelVersion: 1;
+  readonly modelType: 'Sensor';
+  readonly modelVersion: 1;
   // Sensor:
-  sensorId: string; // UUID
-  startTimestamp: number;
-  endTimestamp: number;
-  placementNote: string;
+  readonly sensorId: string; // UUID
+  readonly startTimestamp: number;
+  readonly endTimestamp: number;
+  readonly placementNote: string;
 }
 
 export type SensorValue
@@ -30,40 +30,40 @@ export type SensorValue
 
 export interface DexcomSensorValue {
   // Model:
-  modelType: 'DexcomSensorValue';
-  modelVersion: 1;
+  readonly modelType: 'DexcomSensorValue';
+  readonly modelVersion: 1;
   // SensorValue:
-  timestamp: number;
-  bloodGlucose: number; // in mmol/L (as opposed to mg/dL, as used by Dexcom)
+  readonly timestamp: number;
+  readonly bloodGlucose: number; // in mmol/L (as opposed to mg/dL, as used by Dexcom)
   // DexcomSensorValue:
-  signalStrength: number; // i.e. "rssi"
-  noiseLevel: number;
+  readonly signalStrength: number; // i.e. "rssi"
+  readonly noiseLevel: number;
 }
 
 export interface DexcomRawSensorValue {
   // Model:
-  modelType: 'DexcomRawSensorValue';
-  modelVersion: 1;
+  readonly modelType: 'DexcomRawSensorValue';
+  readonly modelVersion: 1;
   // SensorValue:
-  timestamp: number;
-  bloodGlucose: number; // in mmol/L (as opposed to mg/dL, as used by Dexcom)
+  readonly timestamp: number;
+  readonly bloodGlucose: number; // in mmol/L (as opposed to mg/dL, as used by Dexcom)
   // DexcomRawSensorValue:
-  signalStrength: number; // i.e. "rssi"
-  noiseLevel: number;
-  rawFiltered: number;
-  rawUnfiltered: number;
+  readonly signalStrength: number; // i.e. "rssi"
+  readonly noiseLevel: number;
+  readonly rawFiltered: number;
+  readonly rawUnfiltered: number;
 }
 
 export interface ParakeetSensorValue {
   // Model:
-  modelType: 'ParakeetSensorValue';
-  modelVersion: 1;
+  readonly modelType: 'ParakeetSensorValue';
+  readonly modelVersion: 1;
   // SensorValue:
-  timestamp: number;
-  bloodGlucose: number; // in mmol/L (as opposed to mg/dL, as used by Dexcom)
+  readonly timestamp: number;
+  readonly bloodGlucose: number; // in mmol/L (as opposed to mg/dL, as used by Dexcom)
   // ParakeetSensorValue:
-  rawFiltered: number;
-  rawUnfiltered: number;
+  readonly rawFiltered: number;
+  readonly rawUnfiltered: number;
 }
 
 export type Calibration
@@ -73,73 +73,73 @@ export type Calibration
 
 export interface DexcomCalibration {
   // Model:
-  modelType: 'DexcomCalibration';
-  modelVersion: 1;
+  readonly modelType: 'DexcomCalibration';
+  readonly modelVersion: 1;
   // Calibration:
-  timestamp: number;
-  bloodGlucose: number[];
-  isInitialCalibration: boolean;
-  slope: number;
-  intercept: number;
+  readonly timestamp: number;
+  readonly bloodGlucose: number[];
+  readonly isInitialCalibration: boolean;
+  readonly slope: number;
+  readonly intercept: number;
   // DexcomCalibration:
-  scale: number;
+  readonly scale: number;
 }
 
 export interface NightbearCalibration {
   // Model:
-  modelType: 'NightbearCalibration';
-  modelVersion: 1;
+  readonly modelType: 'NightbearCalibration';
+  readonly modelVersion: 1;
   // Calibration:
-  timestamp: number;
-  bloodGlucose: number[];
-  isInitialCalibration: boolean;
-  slope: number;
-  intercept: number;
+  readonly timestamp: number;
+  readonly bloodGlucose: number[];
+  readonly isInitialCalibration: boolean;
+  readonly slope: number;
+  readonly intercept: number;
   // NightbearCalibration:
-  sensorId: string; // UUID
-  rawValueId: string; // UUID
-  slopeConfidence: number;
+  readonly sensorId: string; // UUID
+  readonly rawValueId: string; // UUID
+  readonly slopeConfidence: number;
 }
 
 export interface DeviceStatus {
   // Model:
-  modelType: 'DeviceStatus';
-  modelVersion: 1;
+  readonly modelType: 'DeviceStatus';
+  readonly modelVersion: 1;
   // DeviceStatus:
-  deviceName: string;
-  timestamp: number;
-  batteryLevel: number;
-  geolocation: string | null;
+  readonly deviceName: string;
+  readonly timestamp: number;
+  readonly batteryLevel: number;
+  readonly geolocation: string | null;
 }
 
 export interface Hba1c {
   // Model:
-  modelType: 'Hba1c';
-  modelVersion: 1;
+  readonly modelType: 'Hba1c';
+  readonly modelVersion: 1;
   // Hba1c:
-  source: 'calculated' | 'measured';
-  timestamp: number;
-  hba1cValue: number;
+  readonly source: 'calculated' | 'measured';
+  readonly timestamp: number;
+  readonly hba1cValue: number;
 }
 
 export interface Insulin {
   // Model:
-  modelType: 'Insulin';
-  modelVersion: 1;
+  readonly modelType: 'Insulin';
+  readonly modelVersion: 1;
   // Insulin:
-  timestamp: number;
-  amount: number;
-  insulinType: string;
+  readonly timestamp: number;
+  readonly amount: number;
+  readonly insulinType: string;
 }
 
 export interface Carbs {
   // Model:
-  modelType: 'Carbs';
-  modelVersion: 1;
+  readonly modelType: 'Carbs';
+  readonly modelVersion: 1;
   // Carbs:
-  timestamp: number;
-  amount: number;
-  carbsType: 'fast' | 'normal' | 'slow';
+  readonly timestamp: number;
+  readonly amount: number;
+  readonly carbsType: 'fast' | 'normal' | 'slow';
 }
 
 export type Situation
@@ -154,49 +154,49 @@ export type Situation
 
 export interface Alarm {
   // Model:
-  modelType: 'Alarm';
-  modelVersion: 1;
+  readonly modelType: 'Alarm';
+  readonly modelVersion: 1;
   // Alarm:
-  creationTimestamp: number;
-  validAfterTimestamp: number;
-  alarmLevel: number;
-  situationType: Situation;
-  isActive: boolean;
-  pushoverReceipts: string[];
+  readonly creationTimestamp: number;
+  readonly validAfterTimestamp: number;
+  readonly alarmLevel: number;
+  readonly situationType: Situation;
+  readonly isActive: boolean;
+  readonly pushoverReceipts: string[];
 }
 
 export interface Settings {
   // Model:
-  modelType: 'Settings';
-  modelVersion: 1;
+  readonly modelType: 'Settings';
+  readonly modelVersion: 1;
   // Settings:
-  alarmsEnabled: boolean;
+  readonly alarmsEnabled: boolean;
 }
 
 export interface Profile {
   // Model:
-  modelType: 'Profile';
-  modelVersion: 1;
+  readonly modelType: 'Profile';
+  readonly modelVersion: 1;
   // Profile:
-  profileName: string;
-  activatedAt: {
-    hours: number;
-    minutes: number;
+  readonly profileName: string;
+  readonly activatedAt: {
+    readonly hours: number;
+    readonly minutes: number;
   };
-  analyserSettings: {
-    HIGH_LEVEL_REL: number;
-    TIME_SINCE_SGV_LIMIT: number;
-    BATTERY_LIMIT: number;
-    LOW_LEVEL_ABS: number;
-    ALARM_EXPIRE: number;
-    LOW_LEVEL_REL: number;
-    HIGH_LEVEL_ABS: number;
-    ALARM_RETRY: number;
+  readonly analyserSettings: {
+    readonly HIGH_LEVEL_REL: number;
+    readonly TIME_SINCE_SGV_LIMIT: number;
+    readonly BATTERY_LIMIT: number;
+    readonly LOW_LEVEL_ABS: number;
+    readonly ALARM_EXPIRE: number;
+    readonly LOW_LEVEL_REL: number;
+    readonly HIGH_LEVEL_ABS: number;
+    readonly ALARM_RETRY: number;
   };
-  alarmSettings: {
-    [S in Situation]: {
-      escalationAfterMinutes: number[];
-      snoozeMinutes: number;
+  readonly alarmSettings: {
+    readonly [S in Situation]: {
+      readonly escalationAfterMinutes: number[];
+      readonly snoozeMinutes: number;
     }
   };
 }
