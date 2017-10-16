29b19b108927d6e42e777869ca618c8dc7204cdb
Marja Käpyaho <marja.kapyaho@futurice.com>
2017-10-16 22:51:53 +0300

    added initial parakeet entry endpoint

--- server/api/getServerStatus.ts
+++ server/api/getServerStatus.ts
@@ -1,6 +1,5 @@
 import { Response, Context, Request, createResponse } from '../utils/api';
 
-// API function for checking the health & status of the server
 export function getServerStatus(request: Request, context: Context): Response {
   return createResponse({
     message: 'Nightbear Server is OK',
