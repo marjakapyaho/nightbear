50fb1b1440ae2ba9d537d6cc54b9dfb5590ca4da
Marja Käpyaho <marja.kapyaho@futurice.com>
2017-10-16 22:51:53 +0300

    added initial parakeet entry endpoint

---
+++ server/api/uploadParakeetEntry.ts
@@ -0,0 +1,6 @@
+import { Response, Request, createResponse } from '../utils/api';
+
+export function uploadParakeetEntry(request: Request): Response {
+  request; // TODO
+  return createResponse('!ACK  0!');
+}
