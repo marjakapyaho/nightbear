4da25a96760cbf5a6d4f6b922ea8bb14e3d7eab0
Marja Käpyaho <marja.kapyaho@futurice.com>
2018-02-24 18:17:54 +0200

    added wip api call for fetching hba1c history

---
+++ server/api/getHba1cHistory/getHba1cHistory.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+
+describe('api/getHba1cHistory', () => {
+
+  // Assertations
+  it('get Hba1c history', () => {
+    console.log('TODO: test fetching hba1c history'); // tslint:disable-line:no-console
+  });
+});
