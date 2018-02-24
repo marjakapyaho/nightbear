4490d48e032b0e80d964753aecc6fbd7804bbb38
Marja Käpyaho <marja.kapyaho@futurice.com>
2018-02-24 18:04:54 +0200

    added wip api call for acking latest alarm

---
+++ server/api/ackLatestAlarm/ackLatestAlarm.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+
+describe('api/ackLatestAlarm', () => {
+
+  // Assertations
+  it('ack latest alarm', () => {
+    console.log('TODO: test acking alarms'); // tslint:disable-line:no-console
+  });
+});
