b90539bf555fb1e78707380a76969b412bc3a725
Marja Käpyaho <marja.kapyaho@futurice.com>
2018-02-24 17:08:31 +0200

    added api call for watch status

---
+++ server/api/getWatchStatus/getWatchStatus.spec.ts
@@ -0,0 +1,44 @@
+import 'mocha';
+import { assert } from 'chai';
+import { getWatchStatus } from './getWatchStatus';
+
+describe('api/getWatchStatus', () => {
+
+  const context: any = {
+    timestamp: () => 1508672249758,
+  };
+
+  // Mock objects
+  const mockResponseJson: object = {
+    alarms: [{
+      modelType: 'Alarm',
+      modelVersion: 1,
+      creationTimestamp: 324234324,
+      validAfterTimestamp: 234432423,
+      alarmLevel: 1,
+      situationType: 'PERSISTENT_HIGH',
+      isActive: true,
+      pushoverReceipts: [],
+    }],
+    deviceStatus: {
+      modelType: 'DeviceStatus',
+      modelVersion: 1,
+      deviceName: 'dexcom',
+      timestamp: 324234324,
+      batteryLevel: 80,
+      geolocation: null,
+    },
+  };
+
+  // Assertations
+  // TODO: this doesn't really test anything
+  it('get watch status', () => {
+    return getWatchStatus(context)
+      .then(res => {
+        assert.deepEqual(
+          res.responseBody,
+          mockResponseJson,
+        );
+      });
+  });
+});
