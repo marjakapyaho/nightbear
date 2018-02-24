d9bf6f3f588df3c1c69e4b5306a5c00528794377
Marja Käpyaho <marja.kapyaho@futurice.com>
2018-02-24 17:08:31 +0200

    added api call for watch status

---
+++ server/api/getWatchStatus/getWatchStatus.ts
@@ -0,0 +1,37 @@
+import { Response, Context, createResponse } from '../../utils/api';
+import { Alarm, DeviceStatus } from '../../utils/model';
+
+export function getWatchStatus(context: Context): Response {
+  return createResponse({
+    alarms: getActiveAlarms(context.timestamp()),
+    deviceStatus: getDeviceStatus(context.timestamp()),
+  });
+}
+
+export function getDeviceStatus(timestamp: number): DeviceStatus {
+  // TODO: use timestamp to get device status
+  console.log(timestamp); // tslint:disable-line:no-console
+  return {
+    modelType: 'DeviceStatus',
+    modelVersion: 1,
+    deviceName: 'dexcom',
+    timestamp: 324234324,
+    batteryLevel: 80,
+    geolocation: null,
+  };
+}
+
+export function getActiveAlarms(timestamp: number): Alarm[] {
+  // TODO: use timestamp to get active alarms
+  console.log(timestamp); // tslint:disable-line:no-console
+  return [{
+    modelType: 'Alarm',
+    modelVersion: 1,
+    creationTimestamp: 324234324,
+    validAfterTimestamp: 234432423,
+    alarmLevel: 1,
+    situationType: 'PERSISTENT_HIGH',
+    isActive: true,
+    pushoverReceipts: [],
+  }];
+}
