0a1bea49e37da598b2346dc7910d0f4ba51b9a1d
Jarno Rantanen <jarno@jrw.fi>
2018-03-04 12:27:56 +0200

    Simplify EC2 setup script.

--- terraform/ec2-setup.sh
+++ terraform/ec2-setup.sh
@@ -8,13 +8,10 @@ sudo sh get-docker.sh
 sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Allow our main user to run Docker commands
-sudo usermod -aG docker ubuntu
-
 # Check out codebase
 git clone https://github.com/marjakapyaho/nightbear.git
 cd nightbear/server
 git checkout add-server-setup
 
 # Launch services
-docker-compose up -d
+sudo docker-compose up -d
