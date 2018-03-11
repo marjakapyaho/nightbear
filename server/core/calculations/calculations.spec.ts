c7df4a3115bad8f80a15d9034411f8ce5cad2f52
Marja Käpyaho <marja.kapyaho@futurice.com>
2018-03-11 14:13:43 +0200

    added tests for calculations

---
+++ server/core/calculations/calculations.spec.ts
@@ -0,0 +1,75 @@
+import { assert } from 'chai';
+import 'mocha';
+import {
+  calculateRaw,
+  changeBloodGlucoseUnitToMgdl,
+  changeBloodGlucoseUnitToMmoll,
+  isDexcomEntryValid,
+  roundTo2Decimals,
+} from './calculations';
+
+describe('core/calculations', () => {
+
+  it('changeBloodGlucoseUnitToMmoll', () => {
+    assert.deepEqual(changeBloodGlucoseUnitToMmoll(160), 8.9);
+    assert.deepEqual(changeBloodGlucoseUnitToMmoll(60), 3.3);
+    assert.deepEqual(changeBloodGlucoseUnitToMmoll(450), 25.0);
+  });
+
+  it('changeBloodGlucoseUnitToMgdl', () => {
+    assert.deepEqual(changeBloodGlucoseUnitToMgdl(2.8), 50);
+    assert.deepEqual(changeBloodGlucoseUnitToMgdl(11.0), 198);
+    assert.deepEqual(changeBloodGlucoseUnitToMgdl(20.8), 374);
+  });
+
+  it('calculateRaw', () => {
+    const testObj1 = {
+      unfiltered: 204960,
+      slope: 681.4329083181542,
+      intercept: 30000,
+      scale: 0.9977203313342593,
+    };
+
+    assert.deepEqual(
+      calculateRaw(testObj1.unfiltered, testObj1.slope, testObj1.intercept, testObj1.scale),
+      14.2, // Dexcom 14.2, old raw 13.3
+    );
+
+    const testObj2 = {
+      unfiltered: 148224,
+      slope: 645.2005532503458,
+      intercept: 30000,
+      scale: 1,
+    };
+
+    assert.deepEqual(
+      calculateRaw(testObj2.unfiltered, testObj2.slope, testObj2.intercept, testObj2.scale),
+      10.2, // Dexcom 9.8, old raw 9.3
+    );
+
+    const testObj3 = {
+      unfiltered: 213408,
+      slope: 0,
+      intercept: 30000,
+      scale: 0.9977203313342593,
+    };
+
+    assert.deepEqual(
+      calculateRaw(testObj3.unfiltered, testObj3.slope, testObj3.intercept, testObj3.scale),
+      null,
+    );
+  });
+
+  it('isDexcomEntryValid', () => {
+    assert.deepEqual(isDexcomEntryValid(4, 10), false); // Too much noise, too low bg
+    assert.deepEqual(isDexcomEntryValid(2, 10), false); // Too low bg
+    assert.deepEqual(isDexcomEntryValid(5, 100), false); // Too much noise
+    assert.deepEqual(isDexcomEntryValid(3, 80), true);
+  });
+
+  it('roundTo2Decimals', () => {
+    assert.deepEqual(roundTo2Decimals(34.0879), 34.09);
+    assert.deepEqual(roundTo2Decimals(5.9999), 6.00);
+    assert.deepEqual(roundTo2Decimals(2.457), 2.46);
+  });
+});
