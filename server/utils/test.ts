2dfea37ae7a2c7d8a1195fd297dff5fa8f85ab2f
Jarno Rantanen <jarno@jrw.fi>
2018-05-10 15:40:11 +0300

    Add in-memory DB support for test runner.

--- server/utils/test.ts
+++ server/utils/test.ts
@@ -1,5 +1,6 @@
 import 'mocha';
 import * as PouchDB from 'pouchdb';
+import * as PouchDBMemory from 'pouchdb-adapter-memory';
 import { Context, Request } from '../models/api';
 import { NO_LOGGING } from './logging';
 import { NO_STORAGE } from '../storage/storage';
@@ -8,10 +9,24 @@ import { Storage } from '../storage/storage';
 import { createCouchDbStorage } from '../storage/couchDbStorage';
 import { getUuid } from './uuid';
 
+PouchDB.plugin(PouchDBMemory); // @see https://www.npmjs.com/package/pouchdb-adapter-memory
+
 export type TestSuite = (storage: () => Storage) => void;
 
+const TEMP_DB_PREFIX = 'temp_test_db_';
+
 export function withStorage(suite: TestSuite) {
+  withInMemoryStorage(suite);
   withCouchDbStorage(suite);
+}
+
+function withInMemoryStorage(suite: TestSuite) {
+  describe('with in-memory storage', () => {
+    suite(() => {
+      const dbUrl = TEMP_DB_PREFIX + getUuid();
+      return createCouchDbStorage(dbUrl, { adapter: 'memory' });
+    });
+  });
 }
 
 function withCouchDbStorage(suite: TestSuite) {
@@ -19,7 +34,7 @@ function withCouchDbStorage(suite: TestSuite) {
   (NIGHTBEAR_TEST_DB_URL ? describe : xdescribe)('with CouchDB storage', () => {
     let createdDbs: string[] = [];
     suite(() => {
-      const dbUrl = `${NIGHTBEAR_TEST_DB_URL}temp_test_db_${getUuid()}`;
+      const dbUrl = NIGHTBEAR_TEST_DB_URL + TEMP_DB_PREFIX + getUuid();
       createdDbs.push(dbUrl);
       return createCouchDbStorage(dbUrl);
     });
