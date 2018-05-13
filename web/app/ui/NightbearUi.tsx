8fda65a4ca95eb4aa2d1e3ff920a441e81ce706f
Jarno Rantanen <jarno@jrw.fi>
2018-05-13 21:28:32 +0300

    Add simple DbStatusBar component.

--- web/app/ui/NightbearUi.tsx
+++ web/app/ui/NightbearUi.tsx
@@ -1,10 +1,12 @@
 import { renderFromStore } from 'app/utils/react';
+import DbStatusBar from 'app/ui/utils/DbStatusBar';
 
 export default renderFromStore(
   __filename,
   state => state.config,
   (React, { remoteDbUrl }, dispatch) => (
     <div>
+      <DbStatusBar />
       {!!remoteDbUrl && <pre>Logged in, with dbUrl = {remoteDbUrl}</pre>}
       <button
         onClick={() =>
---
+++ web/app/ui/utils/DbStatusBar.tsx
@@ -0,0 +1,59 @@
+import { renderFromStore } from 'app/utils/react';
+import { State, ReplicationDirection, ReplicationState } from 'app/reducers';
+import { CSSProperties } from 'react';
+import { assertExhausted } from 'app/utils/types';
+
+export default renderFromStore(
+  __filename,
+  state => state.replication,
+  (React, { UP, DOWN }) => {
+    return (
+      <div className="this">
+        {renderState('UP', UP)}
+        {renderState('DOWN', DOWN)}
+      </div>
+    );
+    function renderState(
+      dir: ReplicationDirection,
+      state: State['replication'][ReplicationDirection],
+    ) {
+      return (
+        <div className="dir" style={getStyle(state.state)}>
+          {dir}: {state.state}
+        </div>
+      );
+    }
+  },
+);
+
+function getStyle(state: ReplicationState): CSSProperties {
+  switch (state) {
+    case 'DISABLED':
+      return {
+        background: 'white',
+        color: 'gray',
+      };
+    case 'ACTIVE':
+      return {
+        background: 'green',
+        color: 'white',
+      };
+    case 'ONLINE':
+      return {
+        background: 'gray',
+        color: 'white',
+      };
+    case 'OFFLINE':
+      return {
+        background: 'orange',
+        color: 'black',
+      };
+    case 'ERROR':
+      return {
+        background: 'darkred',
+        color: 'white',
+      };
+    default:
+      return assertExhausted(state);
+  }
+}
