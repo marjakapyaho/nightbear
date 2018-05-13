9beececec6c8ba48374d127999cae39c800f66e3
Jarno Rantanen <jarno@jrw.fi>
2018-05-13 19:48:44 +0300

    Remove unneeded (?) TypeScript workaround.

--- web/app/reducers/index.tsx
+++ web/app/reducers/index.tsx
@@ -1,7 +1,4 @@
-import { AnyAction } from 'redux';
 import { Action } from 'app/actions';
-
-export type TypeScriptWorkaround = AnyAction; // @see https://github.com/Microsoft/TypeScript/issues/5711
 
 export type State = Readonly<{
   config: {
