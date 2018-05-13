eb14fb955c72a50a1a172ea6cf15d24bbd8651e9
Marja Käpyaho <marja.kapyaho@futurice.com>
2018-05-13 18:59:16 +0300

    working endpoints for calculating and fetching hba1c history

---
+++ server/api/calculateHba1c/calculateHba1c.spec.ts
@@ -0,0 +1,71 @@
+import 'mocha';
+import { assertEqualWithoutMeta, createTestContext, createTestRequest, withStorage } from '../../utils/test';
+import { DexcomCalibration, Hba1c } from '../../models/model';
+import { uploadDexcomEntry } from '../uploadDexcomEntry/uploadDexcomEntry';
+import { Request } from '../../models/api';
+import { calculateHba1cForDate } from './calculateHba1c';
+import { getHba1cHistory } from '../getHba1cHistory/getHba1cHistory';
+
+describe('api/calculateHba1c', () => {
+
+  const request = createTestRequest();
+
+  const mockDexcomCalibration: DexcomCalibration = {
+    modelType: 'DexcomCalibration',
+    timestamp: 1508672249758,
+    meterEntries: [{
+      modelType: 'MeterEntry',
+      timestamp: 1508672249758,
+      bloodGlucose: 8.0,
+      measuredAt: 1508672249758,
+    }],
+    isInitialCalibration: false,
+    slope: 828.3002146147081,
+    intercept: 30000,
+    scale: 0.9980735302684531,
+  };
+
+  const mockRequestBgEntry: Request = {
+    requestId: '',
+    requestMethod: '',
+    requestPath: '',
+    requestHeaders: {},
+    requestParams: {},
+    requestBody: {
+      unfiltered: 158880,
+      filtered: 156608,
+      direction: 'Flat',
+      device: 'dexcom',
+      rssi: 168,
+      sgv: 150, // 8.3 mmol/l
+      dateString: 'Sun Nov 22 23:27:50 EET 2015',
+      type: 'sgv',
+      date: 1448227670000,
+      noise: 1,
+    },
+  };
+
+  withStorage(createTestStorage => {
+
+    it('calculate hba1c for date', () => {
+      const context = createTestContext(createTestStorage());
+
+      const mockHba1c: Hba1c = {
+        modelType: 'Hba1c',
+        source: 'calculated',
+        timestamp: context.timestamp(),
+        hba1cValue: 6.218815331010453,
+      };
+
+      const mockResponseJson = [mockHba1c];
+
+      return Promise.resolve()
+        .then(() => context.storage.saveModel(mockDexcomCalibration))
+        .then(() => uploadDexcomEntry(mockRequestBgEntry, context))
+        .then(() => calculateHba1cForDate(request, context))
+        .then(() => getHba1cHistory(request, context))
+        .then(res => assertEqualWithoutMeta(res.responseBody as any, mockResponseJson));
+    });
+
+  });
+});
