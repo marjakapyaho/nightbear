35956741f23a9e8f255d0a15a55100067ee5d3f8
Marja Käpyaho <marja.kapyaho@futurice.com>
2018-05-17 00:16:38 +0300

    added logging to profile

--- server/app/profile.js
+++ server/app/profile.js
@@ -8,9 +8,11 @@ export default app => {
             log('Getting active profile');
             const currentTimestamp = app.currentTime();
             if (new Date(currentTimestamp).getHours() > 9) { // DAY
+                log('Selected active profile: DAY', new Date(currentTimestamp).getHours());
                 return settingsObj.profiles.day;
             }
             else { // NIGHT
+                log('Selected active profile: NIGHT', new Date(currentTimestamp).getHours());
                 return settingsObj.profiles.night;
             }
         }
