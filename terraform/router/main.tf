44fe8ca10b4a7eccb60d364e80942ae217852a24
Jarno Rantanen <jarno@jrw.fi>
2018-06-02 10:18:30 +0300

    Update lambda-multicast-proxy to 1.1.0.

--- terraform/router/main.tf
+++ terraform/router/main.tf
@@ -2,7 +2,7 @@ module "cloudfront_apigw_lambda" {
   source                                    = "../utils/cloudfront_apigw_lambda"
   cloudfront_apigw_lambda_function_name     = "NightbearRouter"
   cloudfront_apigw_lambda_domain_name       = "${var.router_domain_name}"
-  cloudfront_apigw_lambda_function_filename = "${path.module}/lambda-multicast-proxy-1.0.1.zip" # https://github.com/jareware/lambda-multicast-proxy
+  cloudfront_apigw_lambda_function_filename = "${path.module}/lambda-multicast-proxy-1.1.0.zip" # https://github.com/jareware/lambda-multicast-proxy
   cloudfront_apigw_lambda_domain_zone       = "${var.router_domain_zone}"
 
   cloudfront_apigw_lambda_function_env_vars = {
