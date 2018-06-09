8834f503abd1e26554e17a191b3cba639baa3e38
Jarno Rantanen <jarno@jrw.fi>
2018-06-09 11:10:56 +0300

    Add live ticking to Timestamp util.

--- web/app/ui/utils/Timestamp.tsx
+++ web/app/ui/utils/Timestamp.tsx
@@ -1,17 +1,48 @@
-import { renderFromProps } from 'nightbear/web/app/utils/react';
+import * as React from 'react';
 import { padStart } from 'lodash';
 
-export default renderFromProps<{ ts: number }>(__filename, (React, props) => {
-  const d = new Date(props.ts);
-  const today = formatDate(new Date());
-  const date = formatDate(d);
-  const time = formatTime(d);
-  return (
-    <div className="this" title={d.toString()}>
-      {date === today ? '' : date} {time}
-    </div>
-  );
-});
+const ns = 'TODO';
+
+type Props = { ts: number };
+
+export default class Clock extends React.Component<Props> {
+  timerID: NodeJS.Timer;
+
+  constructor(props: Props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.timerID = global.setInterval(() => this.forceUpdate(), 1000);
+  }
+
+  componentWillUnmount() {
+    global.clearInterval(this.timerID);
+  }
+
+  render() {
+    const d = new Date(this.props.ts);
+    const now = Date.now();
+    const today = formatDate(new Date(now));
+    const date = formatDate(d);
+    const time = formatTime(d);
+    let output = '';
+    if (date !== today) {
+      output = `${date} ${time}`;
+    } else if (d.getTime() > now - 1000 * 60) {
+      output = ((now - d.getTime()) / 1000).toFixed(0) + ' sec ago';
+    } else if (d.getTime() > now - 1000 * 60 * 60) {
+      output = ((now - d.getTime()) / (1000 * 60)).toFixed(1) + ' min ago';
+    } else {
+      output = time;
+    }
+    return (
+      <div className={ns} title={d.toString()}>
+        {output}
+      </div>
+    );
+  }
+}
 
 function formatDate(d: Date) {
   return `${d.getDay()}.${d.getMonth() + 1}.${d.getFullYear()}`;
