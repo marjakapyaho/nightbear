97693e83d3ace839120e883203d4b8b7d618e6f5
Jarno Rantanen <jarno@jrw.fi>
2018-06-13 23:47:31 +0300

    Add action creator convenience utils/types.

--- web/app/utils/redux.tsx
+++ web/app/utils/redux.tsx
@@ -31,3 +31,37 @@ export function configureStore(initialState?: ReduxState, middleware: Middleware
 
   return store as any;
 }
+
+type valueof<T> = T[keyof T];
+type ActionCreatorMap = { [key: string]: (...args: any[]) => object };
+type ActionCreatorMapWithType<T> = { [K in keyof T]: ActionCreatorWithType<T[K], K> };
+
+// prettier-ignore
+type ActionCreatorWithType<A, T> =
+  A extends () => infer R ? () => Readonly<R & { type: T }> :
+  A extends (a1: infer A1) => infer R ? (a1: A1) => Readonly<R & { type: T }> :
+  A extends (a1: infer A1, a2: infer A2) => infer R ? (a1: A1, a2: A2) => Readonly<R & { type: T }> :
+  A extends (a1: infer A1, a2: infer A2, a3: infer A3) => infer R ? (a1: A1, a2: A2, a3: A3) => Readonly<R & { type: T }> :
+  A extends (a1: infer A1, a2: infer A2, a3: infer A3, a4: infer A4) => infer R ? (a1: A1, a2: A2, a3: A3, a4: A4) => Readonly<R & { type: T }> :
+  A extends (a1: infer A1, a2: infer A2, a3: infer A3, a4: infer A4, a5: infer A5) => infer R ? (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => Readonly<R & { type: T }> :
+  A extends (a1: infer A1, a2: infer A2, a3: infer A3, a4: infer A4, a5: infer A5, a6: infer A6) => infer R ? (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => Readonly<R & { type: T }> :
+  A extends (a1: infer A1, a2: infer A2, a3: infer A3, a4: infer A4, a5: infer A5, a6: infer A6, a7: infer A7) => infer R ? (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7) => Readonly<R & { type: T }> :
+  A extends (a1: infer A1, a2: infer A2, a3: infer A3, a4: infer A4, a5: infer A5, a6: infer A6, a7: infer A7, a8: infer A8) => infer R ? (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7, a8: A8) => Readonly<R & { type: T }> :
+  A extends (a1: infer A1, a2: infer A2, a3: infer A3, a4: infer A4, a5: infer A5, a6: infer A6, a7: infer A7, a8: infer A8, a9: infer A9) => infer R ? (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7, a8: A8, a9: A9) => Readonly<R & { type: T }> :
+  A extends (a1: infer A1, a2: infer A2, a3: infer A3, a4: infer A4, a5: infer A5, a6: infer A6, a7: infer A7, a8: infer A8, a9: infer A9, a10: infer A10) => infer R ? (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7, a8: A8, a9: A9, a10: A10) => Readonly<R & { type: T }> :
+  never; // If you need more than 10 arguments... you don't need more arguments. You need refactoring.
+
+// Updates the given map of action creators so that their return value (i.e. the action object)
+// contains a "type" key with the type of each action, as determined by their key in the given map.
+export function actionsWithType<T extends ActionCreatorMap>(map: T): ActionCreatorMapWithType<T> {
+  return Object.keys(map)
+    .map(type => ({
+      [type]: (...args: any[]) => ({ type, ...map[type].apply(null, args) }),
+    }))
+    .reduce((memo, next) => ({ ...memo, ...next }), {} as any);
+}
+
+// Returns the union of all possible return types from the given action creator map.
+export type ActionUnionFrom<T extends ActionCreatorMap> = valueof<
+  { [P in keyof T]: ReturnType<T[P]> }
+>;
