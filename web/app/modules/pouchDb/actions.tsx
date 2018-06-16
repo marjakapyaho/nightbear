03cc301db93e4e6d886b9a8bd652259dbbfd3d99
Jarno Rantanen <jarno@jrw.fi>
2018-06-16 13:16:47 +0300

    Remove accidentally included 'type' params from actions.

--- web/app/modules/pouchDb/actions.tsx
+++ web/app/modules/pouchDb/actions.tsx
@@ -2,22 +2,17 @@ import { ReplicationDirection } from 'web/app/modules/pouchDb/state';
 import { actionsWithType } from 'web/app/utils/redux';
 
 export const pouchDbActions = actionsWithType({
-  DB_EMITTED_READY: () => ({
-    type: 'DB_EMITTED_READY' as 'DB_EMITTED_READY',
-  }),
+  DB_EMITTED_READY: () => ({}),
 
   DB_EMITTED_CHANGE: (change: PouchDB.Core.ChangesResponseChange<{}>) => ({
-    type: 'DB_EMITTED_CHANGE' as 'DB_EMITTED_CHANGE',
     change,
   }),
 
   DB_EMITTED_COMPLETE: (info: PouchDB.Core.ChangesResponse<{}>) => ({
-    type: 'DB_EMITTED_COMPLETE' as 'DB_EMITTED_COMPLETE',
     info,
   }),
 
   DB_EMITTED_ERROR: (err: PouchDB.Core.Error) => ({
-    type: 'DB_EMITTED_ERROR' as 'DB_EMITTED_ERROR',
     err,
   }),
 
@@ -26,7 +21,6 @@ export const pouchDbActions = actionsWithType({
     direction: ReplicationDirection,
     info: PouchDB.Replication.ReplicationResult<{}>,
   ) => ({
-    type: 'REPLICATION_EMITTED_CHANGE' as 'REPLICATION_EMITTED_CHANGE',
     direction,
     info,
   }),
@@ -36,20 +30,17 @@ export const pouchDbActions = actionsWithType({
     direction: ReplicationDirection,
     err: PouchDB.Core.Error | undefined, // Error when e.g. offline, undefined when e.g. all caught up
   ) => ({
-    type: 'REPLICATION_EMITTED_PAUSED' as 'REPLICATION_EMITTED_PAUSED',
     direction,
     err,
   }),
 
   // replicate resumed (e.g. new changes replicating, user went back online)
   REPLICATION_EMITTED_ACTIVE: (direction: ReplicationDirection) => ({
-    type: 'REPLICATION_EMITTED_ACTIVE' as 'REPLICATION_EMITTED_ACTIVE',
     direction,
   }),
 
   // a document failed to replicate (e.g. due to permissions)
   REPLICATION_EMITTED_DENIED: (direction: ReplicationDirection, err: PouchDB.Core.Error) => ({
-    type: 'REPLICATION_EMITTED_DENIED' as 'REPLICATION_EMITTED_DENIED',
     direction,
     err,
   }),
@@ -58,13 +49,11 @@ export const pouchDbActions = actionsWithType({
     direction: ReplicationDirection,
     info: PouchDB.Replication.ReplicationResultComplete<{}>,
   ) => ({
-    type: 'REPLICATION_EMITTED_COMPLETE' as 'REPLICATION_EMITTED_COMPLETE',
     direction,
     info,
   }),
 
   REPLICATION_EMITTED_ERROR: (direction: ReplicationDirection, err: PouchDB.Core.Error) => ({
-    type: 'REPLICATION_EMITTED_ERROR' as 'REPLICATION_EMITTED_ERROR',
     direction,
     err,
   }),
