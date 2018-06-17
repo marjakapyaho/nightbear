d6b2343a944c6234c968ba649fd15c22bb55257b
Jarno Rantanen <jarno@jrw.fi>
2018-06-17 11:00:54 +0300

    Add DB state change tracking via lastChangedAt.

--- web/app/ui/utils/DbStatusBar.tsx
+++ web/app/ui/utils/DbStatusBar.tsx
@@ -3,18 +3,20 @@ import { CSSProperties } from 'react';
 import { assertExhausted, objectKeys } from 'web/app/utils/types';
 import { getSummaryDbStatus, SummaryDbStatus } from 'web/app/modules/pouchDb/getters';
 import { PouchDbState, PouchDbStatePart } from 'web/app/modules/pouchDb/state';
+import Timestamp from 'web/app/ui/utils/Timestamp';
 
 export default renderFromStore(
   __filename,
   state => state,
   (React, { pouchDb }) => {
     const parts = objectKeys(pouchDb);
-    const { summaryStatus, summaryProgress } = getSummaryDbStatus(pouchDb);
+    const { summaryStatus, summaryProgress, summaryLastChangedAt } = getSummaryDbStatus(pouchDb);
     return (
       <div className="this">
         <div className="parts">{parts.map(part => renderState(part, pouchDb[part]))}</div>
         <div className="summary" style={getStyle(summaryStatus)}>
           {summaryStatus} {summaryProgress === null ? null : `(${summaryProgress} %)`}
+          (since <Timestamp ts={summaryLastChangedAt} />)
         </div>
       </div>
     );
