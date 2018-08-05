08a915f9659a41b1420dab3560c295ac59bdc6d2
Jarno Rantanen <jarno@jrw.fi>
2018-08-05 09:03:03 +0300

    Add model type & time range selectors to BgGraphScreen.

--- web/app/ui/screens/BgGraphScreen.tsx
+++ web/app/ui/screens/BgGraphScreen.tsx
@@ -3,12 +3,25 @@ import * as Highcharts from 'highcharts';
 import * as HighchartsReact from 'highcharts-react-official';
 import { isNotNull } from 'server/utils/types';
 import { TimelineModel } from 'core/models/model';
+import ModelTypeSelector from 'web/app/ui/utils/ModelTypeSelector';
+import TimeRangeSelector from 'web/app/ui/utils/TimeRangeSelector';
+import { actions } from 'web/app/modules/actions';
 
 export default renderFromStore(
   __filename,
   state => state.timelineData,
-  (React, state) => (
+  (React, state, dispatch) => (
     <div className="this">
+      <ModelTypeSelector
+        onChange={newType =>
+          dispatch(actions.TIMELINE_FILTERS_CHANGED(state.filters.range, Date.now(), [newType]))
+        }
+      />
+      <TimeRangeSelector
+        onChange={range =>
+          dispatch(actions.TIMELINE_FILTERS_CHANGED(range, Date.now(), state.filters.modelTypes))
+        }
+      />
       {state.status === 'READY' && (
         <HighchartsReact highcharts={Highcharts} options={getOptions(state.models)} />
       )}
