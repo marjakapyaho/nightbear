11bc14a2152f412989215dd7a5a18d26d8dcac89
Jarno Rantanen <jarno@jrw.fi>
2018-08-05 08:03:44 +0300

    Add simple screen selector.

--- web/app/ui/NightbearUi.tsx
+++ web/app/ui/NightbearUi.tsx
@@ -3,17 +3,35 @@ import { assertExhausted } from 'web/app/utils/types';
 import DebugScreen from 'web/app/ui/screens/DebugScreen';
 import DbStatusBar from 'web/app/ui/utils/DbStatusBar';
 import BgGraphScreen from 'web/app/ui/screens/BgGraphScreen';
+import { actions } from 'web/app/modules/actions';
 
 export default renderFromStore(
   __filename,
-  state => state.configVars,
-  (React, { selectedScreen }) => {
+  state => state.uiNavigation,
+  (React, { selectedScreen }, dispatch) => {
     return (
       <div className="this">
         <DbStatusBar />
+        {renderScreenSelector()}
         {renderSelectedScreen()}
       </div>
     );
+
+    function renderScreenSelector() {
+      return (
+        <select
+          onChange={event => dispatch(actions.UI_NAVIGATED(event.target.value as any))}
+          value={selectedScreen}
+        >
+          <option key="BgGraphScreen" value="BgGraphScreen">
+            BgGraphScreen
+          </option>
+          <option key="DebugScreen" value="DebugScreen">
+            DebugScreen
+          </option>
+        </select>
+      );
+    }
 
     function renderSelectedScreen() {
       switch (selectedScreen) {
