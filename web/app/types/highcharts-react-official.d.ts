c25c45ae9599d96ddf232ab51a34a5a0d60d1f94
Jarno Rantanen <jarno@jrw.fi>
2018-08-05 08:26:22 +0300

    Add simplest possible Highcharts integration.

---
+++ web/app/types/highcharts-react-official.d.ts
@@ -0,0 +1,2 @@
+// There's no official types for this package, but most of the meaningful type defs are for the "highcharts" package anyway
+declare module 'highcharts-react-official';
--- web/app/ui/screens/BgGraphScreen.tsx
+++ web/app/ui/screens/BgGraphScreen.tsx
@@ -1,7 +1,24 @@
 import { renderFromStore } from 'web/app/utils/react';
+import * as Highcharts from 'highcharts';
+import * as HighchartsReact from 'highcharts-react-official';
+
+const options: Highcharts.Options = {
+  title: {
+    text: 'My chart',
+  },
+  series: [
+    {
+      data: [1, 2, 3],
+    },
+  ],
+};
 
 export default renderFromStore(
   __filename,
   state => state,
-  React => <div className="this">TODO</div>,
+  React => (
+    <div className="this">
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  ),
 );
