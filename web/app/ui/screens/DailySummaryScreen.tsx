2f03bfe8a898a0a8fcfde0e4031dc1cf4932c6b5
Jarno Rantanen <jarno@jrw.fi>
2018-08-05 11:35:23 +0300

    Add simple stats calculation to DailySummaryScreen.

--- web/app/ui/screens/DailySummaryScreen.tsx
+++ web/app/ui/screens/DailySummaryScreen.tsx
@@ -1,4 +1,9 @@
+import { DateTime } from 'luxon';
 import { renderFromStore } from 'web/app/utils/react';
+import { groupBy, map, mean } from 'lodash';
+import { ParakeetSensorEntry } from 'core/models/model';
+import { is } from 'core/models/utils';
+import { isNotNull } from 'server/utils/types';
 
 export default renderFromStore(
   __filename,
@@ -10,12 +15,20 @@ export default renderFromStore(
           <thead>
             <tr>
               <th>Day</th>
+              <th>Entries</th>
+              <th>Average BG</th>
+              <th>High entries</th>
+              <th>Low entries</th>
             </tr>
           </thead>
           <tbody>
-            {state.models.map((model, i) => (
+            {getDailySummary(state.models.filter(is('ParakeetSensorEntry'))).map((row, i) => (
               <tr key={i}>
-                <td>{model.timestamp}</td>
+                <td>{row.day}</td>
+                <td>{row.entries}</td>
+                <td>{row.averageBg.toFixed(2)}</td>
+                <td>{row.highs}</td>
+                <td>{row.lows}</td>
               </tr>
             ))}
           </tbody>
@@ -24,3 +37,19 @@ export default renderFromStore(
     </div>
   ),
 );
+
+function getDailySummary(models: ParakeetSensorEntry[]) {
+  const data = models
+    .map(model => ({
+      day: DateTime.fromMillis(model.timestamp).toLocaleString(DateTime.DATE_HUGE),
+      bloodGlucose: model.bloodGlucose,
+    }))
+    .filter(x => isNotNull(x.bloodGlucose));
+  return map(groupBy(data, 'day'), (values, day) => ({
+    day,
+    entries: values.length,
+    averageBg: mean(values.map(val => val.bloodGlucose)),
+    highs: values.filter(val => val.bloodGlucose && val.bloodGlucose > 10).length,
+    lows: values.filter(val => val.bloodGlucose && val.bloodGlucose < 4).length,
+  }));
+}
