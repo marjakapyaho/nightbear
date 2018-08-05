d9b7d832c984d5bce1d86c9f4593b8930f388389
Jarno Rantanen <jarno@jrw.fi>
2018-08-05 08:59:21 +0300

    Add simple BG chart.

--- web/app/ui/screens/BgGraphScreen.tsx
+++ web/app/ui/screens/BgGraphScreen.tsx
@@ -1,24 +1,54 @@
 import { renderFromStore } from 'web/app/utils/react';
 import * as Highcharts from 'highcharts';
 import * as HighchartsReact from 'highcharts-react-official';
-
-const options: Highcharts.Options = {
-  title: {
-    text: 'My chart',
-  },
-  series: [
-    {
-      data: [1, 2, 3],
-    },
-  ],
-};
+import { isNotNull } from 'server/utils/types';
+import { TimelineModel } from 'core/models/model';
 
 export default renderFromStore(
   __filename,
-  state => state,
-  React => (
+  state => state.timelineData,
+  (React, state) => (
     <div className="this">
-      <HighchartsReact highcharts={Highcharts} options={options} />
+      {state.status === 'READY' && (
+        <HighchartsReact highcharts={Highcharts} options={getOptions(state.models)} />
+      )}
     </div>
   ),
 );
+
+// https://www.highcharts.com/demo
+// https://api.highcharts.com/highcharts/
+function getOptions(models: TimelineModel[]): Highcharts.Options {
+  return {
+    title: { text: null },
+    xAxis: { type: 'datetime' },
+    series: [
+      {
+        name: 'Dexcom',
+        data: models
+          .map(model => (model.modelType === 'DexcomSensorEntry' ? model : null))
+          .filter(isNotNull)
+          .map(
+            model =>
+              model.bloodGlucose
+                ? ([model.timestamp, model.bloodGlucose] as [number, number])
+                : null,
+          )
+          .filter(isNotNull),
+      },
+      {
+        name: 'Parakeet',
+        data: models
+          .map(model => (model.modelType === 'ParakeetSensorEntry' ? model : null))
+          .filter(isNotNull)
+          .map(
+            model =>
+              model.bloodGlucose
+                ? ([model.timestamp, model.bloodGlucose] as [number, number])
+                : null,
+          )
+          .filter(isNotNull),
+      },
+    ],
+  };
+}
