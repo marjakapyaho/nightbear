b84bca364dd82e5b6ccd373d3a904fa0cace9d1e
Jarno Rantanen <jarno@jrw.fi>
2018-08-30 00:22:59 +0300

    Add significantly more complete migration script.

--- contrib/legacy-db-migration/migrate.ts
+++ contrib/legacy-db-migration/migrate.ts
@@ -9,44 +9,86 @@ import {
   DexcomCalibration,
   MeterEntry,
   ParakeetSensorEntry,
+  Alarm,
+  DeviceStatus,
+  Hba1c,
+  Insulin,
+  Carbs,
 } from 'core/models/model';
 import { changeBloodGlucoseUnitToMmoll } from 'core/calculations/calculations';
-import { createCouchDbStorage, StorageError, isStorageError } from 'core/storage/couchDbStorage';
-import { chunk } from 'lodash';
+import { createCouchDbStorage } from 'core/storage/couchDbStorage';
+import { chunk, flatten } from 'lodash';
 import * as cliProgress from 'cli-progress';
 import { isNotNull } from 'server/utils/types';
+import { is } from 'core/models/utils';
 
 const DB_PASSWORD = '***';
 const BATCH_SIZE = 500; // @50 ~200000 docs takes ~30 min, @500 ~7 min
 
 const bar = new cliProgress.Bar({});
-const sourceDb = new PouchDB(`https://admin:${DB_PASSWORD}@db-prod.nightbear.fi/legacy`);
+const remoteDb = new PouchDB(`https://admin:${DB_PASSWORD}@db-prod.nightbear.fi/legacy`);
+const sourceDb = new PouchDB(`migrate_temp`);
 const targetStorage = createCouchDbStorage(
-  `https://admin:${DB_PASSWORD}@db-stage.nightbear.fi/migrate_test_7`,
+  `https://admin:${DB_PASSWORD}@db-stage.nightbear.fi/migrate_test_10`,
 );
 const warnings: Error[] = [];
-const skipped: string[] = [];
+let nestedIdTotal: number = 0;
+let remainingNestedIds: string[] = [];
 
 main();
 
 function main() {
   Promise.resolve()
+    .then(() => console.warn('Preparing local copy of DB (this may take a long time)'))
+    .then(migrateToLocalDb)
     .then(() => console.warn("Preparing list of ID's to migrate (this may take a long time)"))
     .then(getDocIdsToMigrate)
+    .then(recordNestedModelIds)
     .then(batchDocIds)
     .then(runBatchesSerially)
+    .then(finalizeDb)
     .catch(err => console.warn('\nMigration failed:', err))
-    .then(() => bar.stop())
-    .then(() => skipped.length && console.warn(`${skipped.length} items skipped`))
-    .then(() => warnings.length && console.warn(`${warnings.length} warnings generated`))
-    .then(() => console.log({ warnings, skipped }))
-    .then(() => console.log('ProTip: Run the script with "> migrate.log" to reduce output'));
+    .then(reportFinished);
+}
+
+function migrateToLocalDb() {
+  const downReplication = PouchDB.replicate(remoteDb, sourceDb, {
+    checkpoint: 'target',
+    batch_size: 5000,
+    retry: true,
+  });
+  let total: number = -1;
+  downReplication.on('change', info => {
+    if (total === -1) {
+      total = info.docs_written + (info.pending || 0);
+      bar.start(total, 0);
+    }
+    bar.increment(info.docs.length);
+  });
+  return new Promise((resolve, reject) => {
+    downReplication.on('complete', resolve);
+    downReplication.on('error', reject);
+    downReplication.on('denied', reject);
+  }).then(() => bar.stop());
+}
+
+function reportFinished() {
+  bar.stop();
+  if (warnings.length) console.warn(`${warnings.length} warnings generated`);
+  if (remainingNestedIds.length)
+    console.warn(`${remainingNestedIds.length}/${nestedIdTotal} nested models got left out`);
+  console.log({ warnings, remainingNestedIds });
+  console.log('ProTip: Run the script with "> migrate.log" to reduce output');
 }
 
 function getDocIdsToMigrate() {
-  return sourceDb
-    .allDocs({ startkey: 'sensor-entries/', endkey: 'sensor-entries/_' })
-    .then(res => res.rows.map(row => row.id));
+  return sourceDb.allDocs().then(res => res.rows.map(row => row.id));
+}
+
+function recordNestedModelIds(ids: string[]): string[] {
+  remainingNestedIds = ids.filter(id => !!id.match(/^meter-entries\b/));
+  nestedIdTotal = remainingNestedIds.length;
+  return ids;
 }
 
 function batchDocIds(ids: string[]): string[][] {
@@ -76,52 +118,99 @@ function runBatch(ids: string[]): Promise<any> {
 
 function toModernModels(docs: object[]) {
   return Promise.all(
-    docs
-      .map(doc => {
-        try {
-          return toModernModel(doc);
-        } catch (err) {
-          warnings.push(err);
-          return null;
-        }
-      })
-      .filter(isNotNull),
-  )
-    .then(models => targetStorage.saveModels(models))
-    .catch((err: Error | StorageError) => {
-      if (isStorageError(err)) {
-        skipped.push(...err.saveFailedForModels);
-      } else {
-        throw err;
+    docs.map(doc => {
+      try {
+        return toModernModel(doc);
+      } catch (err) {
+        warnings.push(err);
+        return null;
       }
-    });
+    }),
+  ).then(models => targetStorage.saveModels(flatten(models.filter(isNotNull)), true));
 }
 
-function toModernModel(x: any): Promise<Model> {
-  if (x._id.match(/^sensor-entries/) && x.type === 'sgv' && x.device === 'dexcom') {
-    return Promise.resolve({
+function toModernModel(x: any, nested = false): Promise<Model[] | null> {
+  if (x._id.match(/^_/) || x._id.match(/^sensors\//)) {
+    return Promise.resolve(null);
+  } else if (x._id.match(/^sensor-entries\//) && x.type === 'sgv' && x.device === 'dexcom') {
+    const model: DexcomSensorEntry = {
       modelType: 'DexcomSensorEntry',
       timestamp: x.date,
       bloodGlucose: changeBloodGlucoseUnitToMmoll(x.sgv),
       signalStrength: x.rssi,
       noiseLevel: x.noise,
-    } as DexcomSensorEntry);
-  } else if (x._id.match(/^sensor-entries-raw/) && x.type === 'raw' && x.device === 'parakeet') {
-    return Promise.resolve({
+    };
+    return Promise.resolve([model]);
+  } else if (x._id.match(/^sensor-entries-raw\//) && x.type === 'raw' && x.device === 'parakeet') {
+    const model: ParakeetSensorEntry = {
       modelType: 'ParakeetSensorEntry',
       timestamp: x.date,
       bloodGlucose: x.nb_glucose_value,
       rawFiltered: x.filtered,
       rawUnfiltered: x.unfiltered,
-    } as ParakeetSensorEntry);
-  } else if (x._id.match(/^meter-entries/) && x.type === 'mbg' && x.device === 'dexcom') {
-    return Promise.resolve({
+    };
+    return Promise.resolve([model]);
+  } else if (
+    nested &&
+    x._id.match(/^meter-entries\//) &&
+    x.type === 'mbg' &&
+    x.device === 'dexcom'
+  ) {
+    remainingNestedIds = remainingNestedIds.filter(id => id !== x._id); // remove this from the list of ID's that we expect to nest
+    const model: MeterEntry = {
       modelType: 'MeterEntry',
       timestamp: x.date,
       bloodGlucose: changeBloodGlucoseUnitToMmoll(x.mbg),
-    } as MeterEntry);
-  } else if (x._id.match(/^calibrations\b/) && x.type === 'cal' && x.device === 'dexcom') {
-    const cal = {
+    };
+    return Promise.resolve([model]);
+  } else if (x._id.match(/^alarms\//)) {
+    const ts = new Date(x._id.replace(/.*\//, '')).getTime();
+    const model: Alarm = {
+      modelType: 'Alarm',
+      creationTimestamp: ts,
+      validAfterTimestamp: x.validAfter || ts,
+      alarmLevel: x.level,
+      situationType: x.type,
+      isActive: x.status !== 'inactive',
+      pushoverReceipts: x.pushoverReceipts || [],
+    };
+    return Promise.resolve([model]);
+  } else if (x._id.match(/^device-status\//)) {
+    const ts = new Date(x._id.replace(/.*\//, '')).getTime();
+    const model: DeviceStatus = {
+      modelType: 'DeviceStatus',
+      deviceName: 'dexcom-uploader',
+      timestamp: ts,
+      batteryLevel: x.uploaderBattery,
+      geolocation: null,
+    };
+    return Promise.resolve([model]);
+  } else if (x._id.match(/^device-status-parakeet\//)) {
+    const model1: DeviceStatus = {
+      modelType: 'DeviceStatus',
+      deviceName: 'parakeet',
+      timestamp: x.date,
+      batteryLevel: parseFloat(x.parakeetBattery),
+      geolocation: x.geoLocation,
+    };
+    const model2: DeviceStatus = {
+      modelType: 'DeviceStatus',
+      deviceName: 'dexcom-transmitter',
+      timestamp: x.date,
+      batteryLevel: parseFloat(x.transmitterBattery) + 1, // TODO: This is very hacky
+      geolocation: null,
+    };
+    return Promise.resolve([model1, model2]);
+  } else if (x._id.match(/^hba1c-history\//)) {
+    const model: Hba1c = {
+      modelType: 'Hba1c',
+      source: 'calculated',
+      timestamp: x.calculationTimestamp,
+      hba1cValue: x.hba1c,
+    };
+    return Promise.resolve([model]);
+  } else if (x._id.match(/^calibrations\//) && x.type === 'cal' && x.device === 'dexcom') {
+    const model: DexcomCalibration = {
       modelType: 'DexcomCalibration',
       timestamp: x.date,
       meterEntries: [], // will be attached below
@@ -129,9 +218,57 @@ function toModernModel(x: any): Promise<Model> {
       slope: x.slope,
       intercept: x.intercept,
       scale: x.scale,
-    } as DexcomCalibration;
-    return Promise.resolve(cal);
+    };
+    return Promise.resolve()
+      .then(() =>
+        sourceDb.allDocs({
+          startkey: `meter-entries/${timestampToString(model.timestamp - 1000 * 60)}`,
+          endkey: `meter-entries/${timestampToString(model.timestamp + 1000 * 60)}`,
+          include_docs: true,
+        }),
+      )
+      .then(res => res.rows.map(row => row.doc))
+      .then(docs => Promise.all(docs.filter(isNotNull).map(doc => toModernModel(doc, true))))
+      .then(models => [
+        {
+          ...model,
+          meterEntries: flatten(models.filter(isNotNull)).filter(is('MeterEntry')),
+        },
+      ]);
+  } else if (x._id.match(/^treatments\//)) {
+    const model1: Insulin = {
+      modelType: 'Insulin',
+      timestamp: x.date,
+      insulinType: 'unknown',
+      amount: parseFloat(x.insulin),
+    };
+    const model2: Carbs = {
+      modelType: 'Carbs',
+      timestamp: x.date,
+      amount: parseFloat(x.carbs),
+      carbsType: 'normal',
+    };
+    const model3: MeterEntry = {
+      modelType: 'MeterEntry',
+      timestamp: x.date,
+      bloodGlucose: parseFloat(x.sugar),
+    };
+    return Promise.resolve(
+      [
+        isFinite(model1.amount) ? model1 : null,
+        isFinite(model2.amount) ? model2 : null,
+        isFinite(model3.bloodGlucose) ? model3 : null,
+      ].filter(isNotNull),
+    );
   } else {
     throw new Error(`WARN: Not sure what to do with "${x._id}": ` + JSON.stringify(x, null, 4));
   }
+}
+
+function finalizeDb() {
+  return targetStorage.loadLatestTimelineModels('Alarm').catch(() => null); // this will just trigger an index build; the result can be ignored
+}
+
+function timestampToString(timestamp: number): string {
+  return new Date(timestamp).toISOString();
 }
