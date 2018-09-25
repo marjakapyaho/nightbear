059aef3ea601fd4d87e0befb6d5138b478a5ca22
Jarno Rantanen <jarno@jrw.fi>
2018-09-25 23:31:56 +0300

    Update ModelTypeSelector to allow multi-selections.

--- web/app/ui/utils/ModelTypeSelector.tsx
+++ web/app/ui/utils/ModelTypeSelector.tsx
@@ -1,19 +1,44 @@
 import { renderFromProps } from 'web/app/utils/react';
 import { TimelineModelType } from 'core/models/model';
+import { isNotNull } from 'server/utils/types';
 
-const options: TimelineModelType[] = ['ParakeetSensorEntry', 'DexcomSensorEntry'];
+const options: TimelineModelType[] = [
+  'DexcomSensorEntry',
+  'DexcomRawSensorEntry',
+  'ParakeetSensorEntry',
+  'DexcomCalibration',
+  'NightbearCalibration',
+  'DeviceStatus',
+  'Hba1c',
+  'MeterEntry',
+  'Insulin',
+  'Carbs',
+];
 
-export default renderFromProps<{ onChange: (newValue: TimelineModelType) => void }>(
-  __filename,
-  (React, props) => (
-    <div className="this">
-      <select onChange={event => props.onChange(event.target.value as TimelineModelType)}>
-        {options.map(value => (
-          <option key={value} value={value}>
-            {value}
-          </option>
-        ))}
-      </select>
-    </div>
-  ),
-);
+export default renderFromProps<{
+  multiple?: boolean;
+  onChange: (newValues: TimelineModelType[]) => void;
+}>(__filename, (React, { multiple, onChange }) => (
+  <div className="this">
+    <select
+      multiple={multiple}
+      onChange={event => {
+        if (multiple) {
+          onChange(
+            (Array.prototype.slice.call(event.target.options) as HTMLOptionElement[])
+              .map(option => (option.selected ? (option.value as TimelineModelType) : null))
+              .filter(isNotNull),
+          );
+        } else {
+          onChange([event.target.value as TimelineModelType]);
+        }
+      }}
+    >
+      {options.map(value => (
+        <option key={value} value={value}>
+          {value}
+        </option>
+      ))}
+    </select>
+  </div>
+));
