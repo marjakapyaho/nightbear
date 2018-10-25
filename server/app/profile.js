3e5dbcac05dd92d24c442064d3591fe524c49b78
Marja Käpyaho <marja.kapyaho@futurice.com>
2018-10-25 22:21:39 +0300

    removed redundant logging

--- server/app/profile.js
+++ server/app/profile.js
@@ -16,15 +16,14 @@ export default app => {
         },
 
         getActiveProfile(settingsObj) {
-            log('Getting active profile');
             const currentTimestamp = app.currentTime();
             // utc 6 = fi 9, utc 20 = fi 23
             if (new Date(currentTimestamp).getHours() > 6 && new Date(currentTimestamp).getHours() < 20) { // DAY
-                log('Selected active profile: DAY', new Date(currentTimestamp).getHours());
+                log.debug('Selected active profile: DAY', new Date(currentTimestamp).getHours());
                 return settingsObj.profiles.day;
             }
             else { // NIGHT
-                log('Selected active profile: NIGHT', new Date(currentTimestamp).getHours());
+                log.debug('Selected active profile: NIGHT', new Date(currentTimestamp).getHours());
                 return settingsObj.profiles.night;
             }
         }
