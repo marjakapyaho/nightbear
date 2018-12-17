9356cb30abe91f42515edc8afe7c6d8609686de5
Jarno Rantanen <jarno@jrw.fi>
2018-12-18 00:37:49 +0200

    Add barebones TimelineDebugScreen.

--- web/app/ui/NightbearUi.tsx
+++ web/app/ui/NightbearUi.tsx
@@ -2,6 +2,7 @@ import { assertExhausted } from 'server/utils/types';
 import BgGraphScreen from 'web/app/ui/screens/BgGraphScreen';
 import DailySummaryScreen from 'web/app/ui/screens/DailySummaryScreen';
 import DebugScreen from 'web/app/ui/screens/DebugScreen';
+import TimelineDebugScreen from 'web/app/ui/screens/TimelineDebugScreen';
 import MainNavBar from 'web/app/ui/utils/MainNavBar';
 import { renderFromStore } from 'web/app/utils/react';
 
@@ -20,6 +21,8 @@ export default renderFromStore(
       switch (selectedScreen) {
         case 'DebugScreen':
           return <DebugScreen />;
+        case 'TimelineDebugScreen':
+          return <TimelineDebugScreen />;
         case 'BgGraphScreen':
           return <BgGraphScreen />;
         case 'DailySummaryScreen':
