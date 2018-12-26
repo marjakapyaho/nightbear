0b24b2d5ad616965aa137e96c1c3dbce062121b3
Jarno Rantanen <jarno@jrw.fi>
2018-12-26 14:51:54 +0200

    Remove reducer & state for timelineData.

--- web/app/modules/reducer.tsx
+++ web/app/modules/reducer.tsx
@@ -2,7 +2,6 @@ import { ReduxAction } from 'web/app/modules/actions';
 import { configVarsReducer } from 'web/app/modules/configVars/reducer';
 import { pouchDbReducer } from 'web/app/modules/pouchDb/reducer';
 import { initReduxState, ReduxState } from 'web/app/modules/state';
-import { timelineDataReducer } from 'web/app/modules/timelineData/reducer';
 import { uiNavigationReducer } from 'web/app/modules/uiNavigation/reducer';
 
 export function rootReducer(state: ReduxState = initReduxState, action: ReduxAction): ReduxState {
@@ -10,6 +9,5 @@ export function rootReducer(state: ReduxState = initReduxState, action: ReduxAct
     configVars: configVarsReducer(state.configVars, action, state),
     uiNavigation: uiNavigationReducer(state.uiNavigation, action, state),
     pouchDb: pouchDbReducer(state.pouchDb, action, state),
-    timelineData: timelineDataReducer(state.timelineData, action, state),
   };
 }
