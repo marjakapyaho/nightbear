30e9bb50e003c676ddc60b5cb0ecd6c7058df688
Jarno Rantanen <jarno@jrw.fi>
2019-01-01 20:56:06 +0200

    Remove unnecessary logging & stopPropagation from series click handler.

--- web/app/ui/screens/TimelineDebugScreen.tsx
+++ web/app/ui/screens/TimelineDebugScreen.tsx
@@ -329,9 +329,8 @@ function getOptions(
             click(event) {
               const point = (event as any).point;
               const model: TimelineModel | null = (point && point.model) || null;
-              console.log('Click:', model);
               if (model) dispatch(actions.MODEL_SELECTED_FOR_EDITING(model));
-              return false;
+              return true;
             },
           },
         },
