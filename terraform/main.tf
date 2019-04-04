b454d55c8883f2702630c78c925bb415e7b707fd
Jarno Rantanen <jarno@jrw.fi>
2019-04-04 15:19:15 +0300

    Use github.com/futurice/terraform-utils/aws_lambda_api to implement our router.

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,9 +30,7 @@ module "metrics" {
 }
 
 module "router" {
-  source             = "./router"
-  router_domain_name = "router.nightbear.fi"
-  router_domain_zone = "${aws_route53_zone.main.zone_id}"
+  source = "./router"
 }
 
 module "hosting_stage" {
--- terraform/router/main.tf
+++ terraform/router/main.tf
@@ -1,11 +1,14 @@
-module "cloudfront_apigw_lambda" {
-  source                                    = "../utils/cloudfront_apigw_lambda"
-  cloudfront_apigw_lambda_function_name     = "NightbearRouter"
-  cloudfront_apigw_lambda_domain_name       = "${var.router_domain_name}"
-  cloudfront_apigw_lambda_function_filename = "${path.module}/lambda-multicast-proxy-v1.2.0.zip" # https://github.com/jareware/lambda-multicast-proxy
-  cloudfront_apigw_lambda_domain_zone       = "${var.router_domain_zone}"
+module "router" {
+  # Available inputs: https://github.com/futurice/terraform-utils/tree/master/aws_lambda_api#inputs
+  # Check for updates: https://github.com/futurice/terraform-utils/compare/v8.0...master
+  source = "git::ssh://git@github.com/futurice/terraform-utils.git//aws_lambda_api?ref=v8.0"
 
-  cloudfront_apigw_lambda_function_env_vars = {
+  api_domain             = "router.nightbear.fi"
+  function_zipfile       = "${path.module}/lambda-multicast-proxy-v1.2.0.zip" # https://github.com/jareware/lambda-multicast-proxy
+  lambda_logging_enabled = true
+  https_only             = false                                              # the router receives data from IoT devices incapable of HTTPS
+
+  function_env_vars = {
     LAMBDA_MULTICAST_CONFIG = "${replace(file("${path.module}/lambda-multicast-proxy-config.json"), "/\\n/", "")}" # Lambda won't like newlines in env vars, so strip them out
   }
 }
