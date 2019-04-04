af83e3bf687b36f632e7d7c2d2e01c2ad2b13a34
Jarno Rantanen <jarno@jrw.fi>
2019-04-04 15:20:49 +0300

    Remove now-unused terraform/utils/cloudfront_apigw_lambda.

--- terraform/utils/cloudfront_apigw_lambda/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "router_api_gw_invoke_url" {
-  description = "This URL can be used to invoke the Lambda through the API Gateway"
-  value       = "${aws_api_gateway_deployment.this.invoke_url}"
-}
