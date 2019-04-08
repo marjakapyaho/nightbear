412e15291b617e3e06175d3eaf613a05173e31eb
Jarno Rantanen <jarno@jrw.fi>
2019-04-08 17:34:18 +0300

    Update lambda-multicast-proxy to latest stable.

--- terraform/router/main.tf
+++ terraform/router/main.tf
@@ -1,10 +1,10 @@
 module "router" {
   # Available inputs: https://github.com/futurice/terraform-utils/tree/master/aws_lambda_api#inputs
-  # Check for updates: https://github.com/futurice/terraform-utils/compare/v8.0...master
-  source = "git::ssh://git@github.com/futurice/terraform-utils.git//aws_lambda_api?ref=v8.0"
+  # Check for updates: https://github.com/futurice/terraform-utils/compare/v9.2...master
+  source = "git::ssh://git@github.com/futurice/terraform-utils.git//aws_lambda_api?ref=v9.2"
 
   api_domain             = "router.nightbear.fi"
-  function_zipfile       = "${path.module}/lambda-multicast-proxy-v1.2.0.zip" # https://github.com/jareware/lambda-multicast-proxy
+  function_zipfile       = "${path.module}/lambda-multicast-proxy-v1.2.1.zip" # https://github.com/jareware/lambda-multicast-proxy
   lambda_logging_enabled = true
   https_only             = false                                              # the router receives data from IoT devices incapable of HTTPS
 
