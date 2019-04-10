c37c9fff5eaeb6881a1ef7becbc32b8328ea672c
Jarno Rantanen <jarno@jrw.fi>
2019-04-10 15:42:24 +0300

    Move providers to their own file.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,3 @@
-# Specify the provider and access details
-provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region}"
-}
-
-# https://docs.aws.amazon.com/acm/latest/userguide/acm-services.html
-# "To use an ACM Certificate with CloudFront, you must request or import the certificate in the US East (N. Virginia) region."
-# https://www.terraform.io/docs/configuration/providers.html#multiple-provider-instances
-provider "aws" {
-  alias      = "us_east_1"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "us-east-1"
-}
-
 resource "aws_route53_zone" "main" {
   name = "nightbear.fi"
 }
---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+# Specify the provider and access details
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+# https://docs.aws.amazon.com/acm/latest/userguide/acm-services.html
+# "To use an ACM Certificate with CloudFront, you must request or import the certificate in the US East (N. Virginia) region."
+# https://www.terraform.io/docs/configuration/providers.html#multiple-provider-instances
+provider "aws" {
+  alias      = "us_east_1"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
+}
