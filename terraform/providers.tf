0743e90a7d038eaa49575f8835f85f9126278253
Jarno Rantanen <jarno@jrw.fi>
2019-04-10 15:50:48 +0300

    Pin provider versions.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,6 @@
 # Specify the provider and access details
 provider "aws" {
+  version    = "~> 2.4"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
@@ -9,8 +10,15 @@ provider "aws" {
 # "To use an ACM Certificate with CloudFront, you must request or import the certificate in the US East (N. Virginia) region."
 # https://www.terraform.io/docs/configuration/providers.html#multiple-provider-instances
 provider "aws" {
+  version    = "~> 2.4"
   alias      = "us_east_1"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "us-east-1"
+}
+
+# As per best practices, pin provider versions:
+
+provider "null" {
+  version = "~> 2.1"
 }
