7069f9d85c98ca4c7a68b318eeb4c9ff43ae357c
Jarno Rantanen <jarno@jrw.fi>
2019-04-10 15:35:15 +0300

    Update router to use AWS tags.

--- terraform/router/main.tf
+++ terraform/router/main.tf
@@ -1,7 +1,7 @@
 module "router" {
   # Available inputs: https://github.com/futurice/terraform-utils/tree/master/aws_lambda_api#inputs
-  # Check for updates: https://github.com/futurice/terraform-utils/compare/v9.2...master
-  source = "git::ssh://git@github.com/futurice/terraform-utils.git//aws_lambda_api?ref=v9.2"
+  # Check for updates: https://github.com/futurice/terraform-utils/compare/v9.4...master
+  source = "git::ssh://git@github.com/futurice/terraform-utils.git//aws_lambda_api?ref=v9.4"
 
   api_domain             = "router.nightbear.fi"
   function_zipfile       = "${path.module}/lambda-multicast-proxy-v1.2.1.zip" # https://github.com/jareware/lambda-multicast-proxy
@@ -10,5 +10,10 @@ module "router" {
 
   function_env_vars = {
     LAMBDA_MULTICAST_CONFIG = "${replace(file("${path.module}/lambda-multicast-proxy-config.json"), "/\\n/", "")}" # Lambda won't like newlines in env vars, so strip them out
+  }
+
+  tags = {
+    Nightbear_Component   = "router"
+    Nightbear_Environment = "mixed"
   }
 }
