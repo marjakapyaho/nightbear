e988bfb707c596bf8e3f4cd298e8a2a4c3c1d3d1
Jarno Rantanen <jarno@jrw.fi>
2020-01-09 12:48:51 +0200

    Add placeholder React-Redux PoC UI.

--- web/src/ui/App.tsx
+++ web/src/ui/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import 'web/src/App.scss';
+import { useReduxState } from 'web/src/utils/react';
 
 const App: React.FC = () => {
-  return <div>TODO</div>;
+  const x = useReduxState(state => state.configVars);
+  return <pre>{JSON.stringify(x, null, 2)}</pre>;
 };
 
 export default App;
