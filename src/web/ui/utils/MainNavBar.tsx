a8873ec4aa5d076b9c1dbe08a785f8eb4aafce3d
Jarno Rantanen <jarno@jrw.fi>
2020-01-11 12:11:30 +0200

    Add back <MainNavBar>.

---
+++ src/web/ui/utils/MainNavBar.tsx
@@ -0,0 +1,29 @@
+import { UiNavigationState } from 'web/modules/uiNavigation/state';
+import 'web/ui/utils/MainNavBar.scss';
+import { useCssNs, useReduxDispatch, useReduxState } from 'web/utils/react';
+
+type Props = {};
+
+export default (() => {
+  const { React } = useCssNs(module.id);
+  const selectedScreen = useReduxState(s => s.uiNavigation.selectedScreen);
+  const { UI_NAVIGATED } = useReduxDispatch();
+
+  return (
+    <div className="this">
+      {renderTab('BgGraphScreen', 'Log')}
+      {renderTab('TimelineDebugScreen', 'Debug')}
+    </div>
+  );
+
+  function renderTab(screen: UiNavigationState['selectedScreen'], title: string) {
+    return (
+      <button
+        className={`button ${screen === selectedScreen ? 'button-selected' : ''}`}
+        onClick={() => UI_NAVIGATED(screen)}
+      >
+        {title}
+      </button>
+    );
+  }
+}) as React.FC<Props>;
