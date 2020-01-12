e5334a5c36cf18c855f0030b890ac560515d425f
Jarno Rantanen <jarno@jrw.fi>
2020-01-12 07:55:24 +0200

    Add back <ScrollNumberSelector>.

---
+++ src/web/ui/utils/ScrollNumberSelector.tsx
@@ -0,0 +1,24 @@
+import { range } from 'lodash';
+import 'web/ui/utils/ScrollNumberSelector.scss';
+import { useCssNs } from 'web/utils/react';
+
+type Props = {
+  value?: number;
+  onChange: (newRange: number) => void;
+  min: number;
+  max: number;
+  step: number;
+  centerOn?: number;
+};
+
+export default (props => {
+  const { React } = useCssNs(module.id);
+
+  return (
+    <div className="this">
+      {range(props.min, props.max + props.step, props.step).map((val, i) => (
+        <div key={i}>{val}</div>
+      ))}
+    </div>
+  );
+}) as React.FC<Props>;
