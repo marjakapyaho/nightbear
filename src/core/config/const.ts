f382200213fc875a3f1585f87decf412dac877c8
Jarno Rantanen <jarno@jrw.fi>
2020-01-23 16:59:21 +0200

    WIP: Remove now-unnecessary const-module.

--- src/core/config/const.ts
+++
@@ -1,4 +0,0 @@
-import { HOUR_IN_MS } from 'core/calculations/calculations';
-
-export const PIXELS_PER_HOUR = 100; // when not scaling a timeline dynamically, use this standard width per unit of time, for consistency across the app
-export const PIXELS_PER_MS = PIXELS_PER_HOUR / HOUR_IN_MS; // this is the less intuitive, but more useful version of the above
