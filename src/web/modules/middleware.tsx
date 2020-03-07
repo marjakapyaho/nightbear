5c758794841ece25b9b1a5242450f2d3fd958202
Jarno Rantanen <jarno@jrw.fi>
2020-03-07 17:37:09 +0200

    When the user's parked in BgGraphScreen, auto-refresh the timeline data every 15 sec.

--- src/web/modules/middleware.tsx
+++ src/web/modules/middleware.tsx
@@ -1,4 +1,5 @@
 import { configMiddleware } from 'web/modules/config/middleware';
+import { navigationMiddleware } from 'web/modules/navigation/middleware';
 import { pouchDbMiddleware } from 'web/modules/pouchDb/middleware';
 
-export const middleware = [configMiddleware, pouchDbMiddleware];
+export const middleware = [configMiddleware, navigationMiddleware, pouchDbMiddleware];
