0b4d50e8b0cf4c417cd4d0014c8a03af79e78578
Jarno Rantanen <jarno@jrw.fi>
2020-03-08 13:27:48 +0200

    Ensure ConfigState is persisted in localStorage.

--- src/web/modules/config/actions.tsx
+++ src/web/modules/config/actions.tsx
@@ -1,7 +1,8 @@
+import { ConfigState } from 'web/modules/config/state';
 import { actionsWithType } from 'web/utils/redux';
 
 export const configActions = actionsWithType({
-  DB_URL_SET: (newDbUrl: string) => ({ newDbUrl }),
+  CONFIG_UPDATED: (newConfig: Partial<ConfigState>) => ({ newConfig }),
   ROLLING_ANALYSIS_TOGGLED: () => ({}),
   AUTO_REFRESH_TOGGLED: () => ({}),
 });
