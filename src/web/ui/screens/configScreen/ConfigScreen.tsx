3ab407643f846b10e856c60df56d1bbc454e95cf
Marja Käpyaho <marja.kapyaho@futurice.com>
2020-04-04 16:22:17 +0300

    added a place in ui for a list of profiles

--- src/web/ui/screens/configScreen/ConfigScreen.tsx
+++ src/web/ui/screens/configScreen/ConfigScreen.tsx
@@ -30,6 +30,47 @@ export default (() => {
         <input type="checkbox" checked={configState.zoomedInTimeline} onChange={actions.ZOOMED_IN_TIMELINE_TOGGLED} />
         Zoomed in timeline
       </label>
+
+      <div
+        style={{
+          padding: 20,
+        }}
+      >
+        <h1>Profiles</h1>
+        <div>
+          <div
+            style={{
+              padding: 20,
+              background: '#7a7a7a',
+              color: 'white',
+              cursor: 'pointer',
+              marginBottom: 10,
+            }}
+          >
+            Day
+          </div>
+          <div
+            style={{
+              padding: 20,
+              background: 'whitesmoke',
+              cursor: 'pointer',
+              marginBottom: 10,
+            }}
+          >
+            Night
+          </div>
+          <div
+            style={{
+              padding: 20,
+              background: 'whitesmoke',
+              cursor: 'pointer',
+              marginBottom: 10,
+            }}
+          >
+            OFF
+          </div>
+        </div>
+      </div>
     </div>
   );
 }) as React.FC<Props>;
