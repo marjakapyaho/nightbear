94ccc70cfc8b91503b54ecea73f9a0f8d3497ebb
Jarno Rantanen <jarno@jrw.fi>
2020-04-11 20:49:29 +0300

    Remove deprecated model editing support from <TimelineDebugScreen> which was never fixed after the big timeline refactoring.

--- src/web/ui/screens/timelineDebugScreen/TimelineDebugScreen.tsx
+++ src/web/ui/screens/timelineDebugScreen/TimelineDebugScreen.tsx
@@ -1,6 +1,4 @@
 import { TimelineModel } from 'core/models/model';
-import { isTimelineModel } from 'core/models/utils';
-import ReactModal from 'react-modal';
 import { DataState } from 'web/modules/data/state';
 import { NavigationState } from 'web/modules/navigation/state';
 import ModelTypeSelector from 'web/ui/components/modelTypeSelector/ModelTypeSelector';
@@ -13,7 +11,7 @@ import { useCssNs, useReduxActions, useReduxState } from 'web/utils/react';
 type Props = {};
 
 export default () => {
-  const { React, ns: cssNs } = useCssNs('TimelineDebugScreen');
+  const { React } = useCssNs('TimelineDebugScreen');
   const navigationState = useReduxState(s => s.navigation);
   const dataState = useReduxState(s => s.data);
   const actions = useReduxActions();
@@ -21,40 +19,7 @@ export default () => {
   if (navigationState.selectedScreen !== 'TimelineDebugScreen') return null; // this screen can only be rendered if it's been selected in state
 
   return (
-    <div className="this" onClick={() => actions.TIMELINE_CURSOR_UPDATED(null)}>
-      {navigationState.modelUuidBeingEdited && (
-        <ReactModal
-          isOpen
-          ariaHideApp={false}
-          onRequestClose={() => actions.MODEL_SELECTED_FOR_EDITING(null)}
-          portalClassName={cssNs('modalPortal')}
-          overlayClassName={cssNs('modalOverlay')}
-          className={cssNs('modalContent')}
-        >
-          <textarea
-            className="model"
-            defaultValue="TODO: Model editing not implemented after the Timeline refactoring, until actually needed"
-          />
-          <button onClick={() => actions.MODEL_SELECTED_FOR_EDITING(null)}>Cancel</button>
-          <button
-            onClick={() => {
-              const txt: HTMLTextAreaElement | null = document.querySelector('.' + cssNs('model')); // TODO: Use React ref here instead
-              if (!txt) return;
-              if (!window.confirm('Are you sure you want to save these changes?')) return;
-              console.log('New model content:', txt.value);
-              const newModel = JSON.parse(txt.value);
-              if (isTimelineModel(newModel)) {
-                actions.MODEL_UPDATED_BY_USER(newModel);
-                actions.MODEL_SELECTED_FOR_EDITING(null);
-              } else {
-                throw new Error(`Couldn't parse content as a TimelineModel`);
-              }
-            }}
-          >
-            Save
-          </button>
-        </ReactModal>
-      )}
+    <div className="this">
       <button
         onClick={() =>
           actions.TIMELINE_FILTERS_CHANGED(
