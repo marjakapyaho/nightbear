aeab669318460f6b3a7d630e614a6a85b9c7bdff
Jarno Rantanen <jarno@jrw.fi>
2020-04-12 22:26:39 +0300

    Upgrade router runtime to node 12.x, because the old one wasn't supported anymore.

--- terraform/router/main.tf
+++ terraform/router/main.tf
@@ -7,6 +7,7 @@ module "router" {
   function_zipfile       = "${path.module}/lambda-multicast-proxy-v1.2.1.zip" # https://github.com/jareware/lambda-multicast-proxy
   lambda_logging_enabled = true                                               # note that this only includes messages from the runtime (e.g. start & stop); the JS code logs directly to Papertrail
   https_only             = false                                              # the router receives data from IoT devices incapable of HTTPS
+  function_runtime       = "nodejs12.x"
 
   function_env_vars = {
     LAMBDA_MULTICAST_CONFIG = "${replace(file("${path.module}/lambda-multicast-proxy-config.json"), "/\\n/", "")}" # Lambda won't like newlines in env vars, so strip them out
