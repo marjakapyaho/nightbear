7861f83583535bb8b10df0619cb0719f8fdf7aca
Jarno Rantanen <jarno@jrw.fi>
2020-04-13 17:00:04 +0300

    Add aws_static_site module.

---
+++ infra/modules/aws_static_site/main.tf
@@ -0,0 +1,28 @@
+module "aws_reverse_proxy" {
+  source    = "git::ssh://git@github.com/futurice/symptomradar.git//infra/modules/aws_reverse_proxy?ref=v2.1"
+  providers = { aws.us_east_1 = aws.us_east_1 } # this alias is needed because ACM is only available in the "us-east-1" region
+
+  # S3 website endpoints are only available over plain HTTP
+  origin_url = "http://${aws_s3_bucket.this.website_endpoint}/"
+
+  # Somewhat perplexingly, this is the "correct" way to ensure users can't bypass CloudFront on their way to S3 resources
+  # https://abridge2devnull.com/posts/2018/01/restricting-access-to-a-cloudfront-s3-website-origin/
+  origin_custom_header_name  = "User-Agent"
+  origin_custom_header_value = random_string.s3_read_password.result
+
+  site_domain            = var.site_domain
+  name_prefix            = var.name_prefix
+  comment_prefix         = var.comment_prefix
+  cloudfront_price_class = var.cloudfront_price_class
+  viewer_https_only      = var.viewer_https_only
+  cache_ttl_override     = var.cache_ttl_override
+  default_root_object    = var.default_root_object
+  add_response_headers   = var.add_response_headers
+  hsts_max_age           = var.hsts_max_age
+  basic_auth_username    = var.basic_auth_username
+  basic_auth_password    = var.basic_auth_password
+  basic_auth_realm       = var.basic_auth_realm
+  basic_auth_body        = var.basic_auth_body
+  lambda_logging_enabled = var.lambda_logging_enabled
+  tags                   = var.tags
+}
---
+++ infra/modules/aws_static_site/variables.tf
@@ -0,0 +1,75 @@
+variable "site_domain" {
+  description = "Domain on which the static site will be made available (e.g. `\"www.example.com\"`)"
+}
+
+variable "name_prefix" {
+  description = "Name prefix to use for objects that need to be created (only lowercase alphanumeric characters and hyphens allowed, for S3 bucket name compatibility)"
+  default     = "aws-static-site"
+}
+
+variable "comment_prefix" {
+  description = "This will be included in comments for resources that are created"
+  default     = "Static site: "
+}
+
+variable "cloudfront_price_class" {
+  description = "CloudFront price class to use (`100`, `200` or `\"All\"`, see https://aws.amazon.com/cloudfront/pricing/)"
+  default     = 100
+}
+
+variable "viewer_https_only" {
+  description = "Set this to `false` if you need to support insecure HTTP access for clients, in addition to HTTPS"
+  default     = true
+}
+
+variable "cache_ttl_override" {
+  description = "When >= 0, override the cache behaviour for ALL objects in S3, so that they stay in the CloudFront cache for this amount of seconds"
+  default     = -1
+}
+
+variable "default_root_object" {
+  description = "The object to return when the root URL is requested"
+  default     = "index.html"
+}
+
+variable "add_response_headers" {
+  description = "Map of HTTP headers (if any) to add to outgoing responses before sending them to clients"
+  type        = "map"
+  default     = {}
+}
+
+variable "hsts_max_age" {
+  description = "How long should `Strict-Transport-Security` remain in effect for the site; disabled automatically when `viewer_https_only = false`"
+  default     = 31557600 # i.e. one year in seconds
+}
+
+variable "basic_auth_username" {
+  description = "When non-empty, require this username with HTTP Basic Auth"
+  default     = ""
+}
+
+variable "basic_auth_password" {
+  description = "When non-empty, require this password with HTTP Basic Auth"
+  default     = ""
+}
+
+variable "basic_auth_realm" {
+  description = "When using HTTP Basic Auth, this will be displayed by the browser in the auth prompt"
+  default     = "Authentication Required"
+}
+
+variable "basic_auth_body" {
+  description = "When using HTTP Basic Auth, and authentication has failed, this will be displayed by the browser as the page content"
+  default     = "Unauthorized"
+}
+
+variable "lambda_logging_enabled" {
+  description = "When true, writes information about incoming requests to the Lambda function's CloudWatch group"
+  default     = false
+}
+
+variable "tags" {
+  description = "AWS Tags to add to all resources created (where possible); see https://aws.amazon.com/answers/account-management/aws-tagging-strategies/"
+  type        = map(string)
+  default     = {}
+}
