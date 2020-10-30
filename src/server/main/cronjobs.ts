77a5c4b13f3e13674173c07801e0837d7d3e08b7
Jarno Rantanen <jarno@jrw.fi>
2020-10-30 23:16:58 +0200

    Start cronjobs main module.

--- src/server/main/cronjobs.ts
+++ src/server/main/cronjobs.ts
@@ -1,7 +1,26 @@
 import { Context } from 'core/models/api';
+import { TZ } from 'core/utils/time';
+import { DateTime } from 'luxon';
+import { MIN_IN_MS } from 'core/calculations/calculations';
+import { extendLogger } from 'core/utils/logging';
 
-export type Cronjob = (context: Context, previousExecutionTime: number) => Promise<unknown>;
+const CRONJOBS_EVERY_MINUTES = 2;
 
-export function startRunningCronjobs(_context: Context, ..._cronjobs: Cronjob[]) {
-  // TODO
+export type Cronjob = (context: Context, executionTimestamps: { then: number; now: number }) => Promise<unknown>;
+
+export function startRunningCronjobs(context: Context, ...cronjobs: Cronjob[]) {
+  const log = extendLogger(context.log, 'cron');
+  log(`System timezone is "${DateTime.local().zoneName}", app timezone is "${TZ}"`);
+  const run = () => {
+    const now = Date.now();
+    return Promise.resolve()
+      .then(() => context.cronjobsJournal.getPreviousExecutionTime())
+      .then(then => {
+        log(`Running, ${((now - then) / MIN_IN_MS).toFixed(1)} min since last run`);
+        // TODO
+        return context.cronjobsJournal.setPreviousExecutionTime(now);
+      });
+  };
+  setInterval(run, CRONJOBS_EVERY_MINUTES * MIN_IN_MS);
+  run();
 }
