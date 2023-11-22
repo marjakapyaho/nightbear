e072e9e02a1395bab69bf4a9c660d4c9e62460c0
Jarno Rantanen <jarno@jrw.fi>
2023-11-22 11:41:27 +0200

    Updates on top of the template.

--- fitbit/nightbear-clockface-panic/companion/index.js
+++ fitbit/nightbear-clockface-panic/companion/index.js
@@ -1,1 +1,42 @@
-console.log('Hello world!');
+import { me } from "companion";
+import { NightbearApi } from "./nightbearApi.js";
+import * as util from "../common/utils";
+import { outbox } from "file-transfer";
+import { encode } from 'cbor';
+import { peerSocket } from "messaging";
+
+const MIN_IN_MILLIS = 60*1000
+const nightbearApi = new NightbearApi()
+let latestData
+let manualInterval
+
+// TODO: move this
+me.wakeInterval = 5 * MIN_IN_MILLIS
+
+if (manualInterval) {
+  self.clearInterval(manualInterval)
+}
+
+self.setTimeout(fetchData, 1000)
+manualInterval = self.setInterval(fetchData, MIN_IN_MILLIS)
+
+peerSocket.onmessage = function(evt) {
+  fetchData()
+}
+
+function fetchData() {
+  console.log('COM: fetching new data')
+  Promise.all([
+    nightbearApi.getBloodSugars(),
+    nightbearApi.getStatus()
+  ])
+    .then(function(data) {
+      outbox.enqueue("bloodsugardata.json", encode({
+        bloodSugarArray: util.getBloodSugarArray(data[0]),
+        status: data[1]
+      }));
+  }).catch(function (e) {
+    console.log("COM: error fetching data") 
+    console.log(e)
+  })
+}
