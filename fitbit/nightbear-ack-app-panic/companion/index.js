06a99f012b29d202c0ff31c65b9aa12a7ecda8fd
Jarno Rantanen <jarno@jrw.fi>
2023-12-09 14:18:04 +0200

    Same, but for the ack-app.

---
+++ fitbit/nightbear-ack-app-panic/companion/index.js
@@ -0,0 +1,39 @@
+import * as messaging from 'messaging';
+
+const API_ACK_ALARM = 'https://router.nightbear.fi/ack-latest-alarm';
+
+messaging.peerSocket.onmessage = function(evt) {
+  if (evt.data.ack) {
+    ackAlarm()
+      .then(function(response) {
+        messaging.peerSocket.send('Success');
+      })
+      .catch(function(error) {
+        messaging.peerSocket.send('Ack failed');
+        console.log(error);
+      });
+  }
+};
+
+function ackAlarm() {
+  return new Promise(function(resolve, reject) {
+    fetch(API_ACK_ALARM, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ acknowledged_by: 'fitbit' }),
+    })
+      .then(function(response) {
+        return response;
+      })
+      .then(function(response) {
+        resolve(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+        reject(error);
+      });
+  });
+}
