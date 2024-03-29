b8182c5f99e82ea0b1526c742f3c0f2b368ab866
Jarno Rantanen <jarno@jrw.fi>
2024-03-29 10:29:23 +0200

    Init TF project.

---
+++ infra/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "~> 1.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.43.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4.2"
+    }
+  }
+}
