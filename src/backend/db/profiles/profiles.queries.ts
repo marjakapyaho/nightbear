84baef2b9e815343e3044849a24e600fc4874cbf
Marja Käpyaho <marja.kapyaho@futurice.com>
2024-04-27 23:05:32 +0300

    get profile query

--- src/backend/db/profiles/profiles.queries.ts
+++ src/backend/db/profiles/profiles.queries.ts
@@ -3,6 +3,8 @@ import { PreparedQuery } from '@pgtyped/runtime';
 
 export type situation = 'BAD_HIGH' | 'BAD_LOW' | 'COMPRESSION_LOW' | 'CRITICAL_OUTDATED' | 'FALLING' | 'HIGH' | 'LOW' | 'OUTDATED' | 'PERSISTENT_HIGH' | 'RISING';
 
+export type Json = null | boolean | number | string | Json[] | { [key: string]: Json };
+
 export type numberArray = (number)[];
 
 export type stringArray = (string)[];
@@ -158,5 +160,81 @@ const createSituationSettingsIR: any = {"usedParamSet":{"situation":true,"profil
  * ```
  */
 export const createSituationSettings = new PreparedQuery<ICreateSituationSettingsParams,ICreateSituationSettingsResult>(createSituationSettingsIR);
+
+
+/** 'GetProfiles' parameters type */
+export type IGetProfilesParams = void;
+
+/** 'GetProfiles' return type */
+export interface IGetProfilesResult {
+  alarmsEnabled: boolean;
+  analyserSettings: Json | null;
+  id: string;
+  isActive: boolean;
+  notificationTargets: stringArray;
+  profileName: string | null;
+  situationSettings: Json | null;
+}
+
+/** 'GetProfiles' query type */
+export interface IGetProfilesQuery {
+  params: IGetProfilesParams;
+  result: IGetProfilesResult;
+}
+
+const getProfilesIR: any = {"usedParamSet":{},"params":[],"statement":"WITH\n  analyser_settings_query AS (\n    SELECT\n      analyser_settings.id,\n      json_build_object(\n        'id', analyser_settings.id::VARCHAR,\n        'highLevelRel', analyser_settings.high_level_rel,\n        'highLevelAbs', analyser_settings.high_level_abs,\n        'highLevelBad', analyser_settings.high_level_bad,\n        'lowLevelRel', analyser_settings.low_level_rel,\n        'lowLevelAbs', analyser_settings.low_level_abs,\n        'lowLevelBad', analyser_settings.low_level_bad,\n        'timeSinceBgMinutes', analyser_settings.time_since_bg_minutes,\n        'highCorrectionSuppressionMinutes', analyser_settings.high_correction_suppression_minutes\n      ) AS analyser_settings\n    FROM analyser_settings\n  ),\n  situation_settings_query AS (\n    SELECT\n      situation_settings.profile_template_id,\n      json_agg(json_build_object(\n         'situation', situation_settings.situation,\n         'escalationAfterMinutes', situation_settings.escalation_after_minutes,\n         'snoozeMinutes', situation_settings.snooze_minutes\n       )) AS situation_settings\n    FROM situation_settings\n    GROUP BY situation_settings.profile_template_id\n  ),\n  most_recent_activation_query AS (\n    SELECT profile_template_id\n    FROM profiles_activations\n    ORDER BY activated_at DESC\n    LIMIT 1\n  )\nSELECT\n  profile_templates.id AS id,\n  profile_name,\n  alarms_enabled,\n  notification_targets,\n  most_recent_activation_query.profile_template_id IS NOT NULL AS \"is_active!\",\n  analyser_settings_query.analyser_settings AS analyser_settings,\n  situation_settings_query.situation_settings AS situation_settings\nFROM profile_templates\n  INNER JOIN analyser_settings_query ON analyser_settings_query.id = profile_templates.analyser_settings_id\n  INNER JOIN situation_settings_query ON situation_settings_query.profile_template_id = profile_templates.id\n  LEFT JOIN most_recent_activation_query ON most_recent_activation_query.profile_template_id = profile_templates.id"};
+
+/**
+ * Query generated from SQL:
+ * ```
+ * WITH
+ *   analyser_settings_query AS (
+ *     SELECT
+ *       analyser_settings.id,
+ *       json_build_object(
+ *         'id', analyser_settings.id::VARCHAR,
+ *         'highLevelRel', analyser_settings.high_level_rel,
+ *         'highLevelAbs', analyser_settings.high_level_abs,
+ *         'highLevelBad', analyser_settings.high_level_bad,
+ *         'lowLevelRel', analyser_settings.low_level_rel,
+ *         'lowLevelAbs', analyser_settings.low_level_abs,
+ *         'lowLevelBad', analyser_settings.low_level_bad,
+ *         'timeSinceBgMinutes', analyser_settings.time_since_bg_minutes,
+ *         'highCorrectionSuppressionMinutes', analyser_settings.high_correction_suppression_minutes
+ *       ) AS analyser_settings
+ *     FROM analyser_settings
+ *   ),
+ *   situation_settings_query AS (
+ *     SELECT
+ *       situation_settings.profile_template_id,
+ *       json_agg(json_build_object(
+ *          'situation', situation_settings.situation,
+ *          'escalationAfterMinutes', situation_settings.escalation_after_minutes,
+ *          'snoozeMinutes', situation_settings.snooze_minutes
+ *        )) AS situation_settings
+ *     FROM situation_settings
+ *     GROUP BY situation_settings.profile_template_id
+ *   ),
+ *   most_recent_activation_query AS (
+ *     SELECT profile_template_id
+ *     FROM profiles_activations
+ *     ORDER BY activated_at DESC
+ *     LIMIT 1
+ *   )
+ * SELECT
+ *   profile_templates.id AS id,
+ *   profile_name,
+ *   alarms_enabled,
+ *   notification_targets,
+ *   most_recent_activation_query.profile_template_id IS NOT NULL AS "is_active!",
+ *   analyser_settings_query.analyser_settings AS analyser_settings,
+ *   situation_settings_query.situation_settings AS situation_settings
+ * FROM profile_templates
+ *   INNER JOIN analyser_settings_query ON analyser_settings_query.id = profile_templates.analyser_settings_id
+ *   INNER JOIN situation_settings_query ON situation_settings_query.profile_template_id = profile_templates.id
+ *   LEFT JOIN most_recent_activation_query ON most_recent_activation_query.profile_template_id = profile_templates.id
+ * ```
+ */
+export const getProfiles = new PreparedQuery<IGetProfilesParams,IGetProfilesResult>(getProfilesIR);
 
 
--- src/backend/db/profiles/profiles.sql
+++ src/backend/db/profiles/profiles.sql
@@ -50,3 +50,53 @@ VALUES (
    :snoozeMinutes!
 )
 RETURNING *;
+
+/*
+  @name getProfiles
+*/
+WITH
+  analyser_settings_query AS (
+    SELECT
+      analyser_settings.id,
+      json_build_object(
+        'id', analyser_settings.id::VARCHAR,
+        'highLevelRel', analyser_settings.high_level_rel,
+        'highLevelAbs', analyser_settings.high_level_abs,
+        'highLevelBad', analyser_settings.high_level_bad,
+        'lowLevelRel', analyser_settings.low_level_rel,
+        'lowLevelAbs', analyser_settings.low_level_abs,
+        'lowLevelBad', analyser_settings.low_level_bad,
+        'timeSinceBgMinutes', analyser_settings.time_since_bg_minutes,
+        'highCorrectionSuppressionMinutes', analyser_settings.high_correction_suppression_minutes
+      ) AS analyser_settings
+    FROM analyser_settings
+  ),
+  situation_settings_query AS (
+    SELECT
+      situation_settings.profile_template_id,
+      json_agg(json_build_object(
+         'situation', situation_settings.situation,
+         'escalationAfterMinutes', situation_settings.escalation_after_minutes,
+         'snoozeMinutes', situation_settings.snooze_minutes
+       )) AS situation_settings
+    FROM situation_settings
+    GROUP BY situation_settings.profile_template_id
+  ),
+  most_recent_activation_query AS (
+    SELECT profile_template_id
+    FROM profiles_activations
+    ORDER BY activated_at DESC
+    LIMIT 1
+  )
+SELECT
+  profile_templates.id AS id,
+  profile_name,
+  alarms_enabled,
+  notification_targets,
+  most_recent_activation_query.profile_template_id IS NOT NULL AS "is_active!",
+  analyser_settings_query.analyser_settings AS analyser_settings,
+  situation_settings_query.situation_settings AS situation_settings
+FROM profile_templates
+  INNER JOIN analyser_settings_query ON analyser_settings_query.id = profile_templates.analyser_settings_id
+  INNER JOIN situation_settings_query ON situation_settings_query.profile_template_id = profile_templates.id
+  LEFT JOIN most_recent_activation_query ON most_recent_activation_query.profile_template_id = profile_templates.id;
