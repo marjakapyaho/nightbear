8db9da423a7c98467459c0e993d6ac56579338e6
Jarno Rantanen <jarno@jrw.fi>
2024-05-09 22:06:23 +0300

    Fix frontend API request headers.

--- src/frontend/data/fetch.ts
+++ src/frontend/data/fetch.ts
@@ -3,6 +3,7 @@ const API_ROOT = 'http://localhost:4000';
 export const callFetch = async <T>(route: string, method = 'GET', bodyObj?: T) => {
   return fetch(`${API_ROOT}${route}`, {
     method,
+    headers: bodyObj ? { 'Content-Type': 'application/json' } : undefined,
     body: JSON.stringify(bodyObj),
   }).then((res: Response) => {
     if (!res.ok) {
