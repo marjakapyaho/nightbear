e09919cd10f5df894b1b9c347a4fa5d52d002956
Jarno Rantanen <jarno@jrw.fi>
2024-05-09 10:23:45 +0300

    Potentially final DB queries API.

--- src/backend/db/queries.ts
+++ src/backend/db/queries.ts
@@ -12,44 +12,65 @@ import { z } from 'zod';
 export const queries = (pool: Pool) => {
   return {
     async createSensorEntry(params: Omit<SensorEntry, 'timestamp'>) {
-      const res = await createSensorEntry.run(params, pool);
-      const mapped = res.map(row => _.mapKeys(row, (_val, key) => _.camelCase(key)));
-      return mapped[0] as unknown as (typeof res)[number];
+      return one(SensorEntry, createSensorEntry, params);
     },
 
     async getLatestSensorEntry() {
-      return query(pool, true, SensorEntry, getLatestSensorEntry);
+      return one(SensorEntry, getLatestSensorEntry);
     },
 
-    async getSensorEntriesByTimestamp(params: { from: string; to?: string }) {
-      return query(pool, false, SensorEntry, getSensorEntriesByTimestamp, params);
+    async getSensorEntriesByTimestamp(params: GenParams<typeof getSensorEntriesByTimestamp>) {
+      return many(SensorEntry, getSensorEntriesByTimestamp, params);
     },
   };
+
+  async function one<
+    Schema extends z.ZodTypeAny,
+    Params extends object,
+    Result extends z.infer<Schema>, // note: the "extends" constraint can be dropped if we just want to rely on the Zod schema, and ignore DB-side types completely
+  >(
+    schema: Schema,
+    method: PreparedQuery<Params, Result>,
+    params: Params,
+  ): Promise<z.infer<Schema>>;
+  async function one<
+    Schema extends z.ZodTypeAny,
+    Params extends void,
+    Result extends z.infer<Schema>,
+  >(schema: Schema, method: PreparedQuery<Params, Result>): Promise<z.infer<Schema>>;
+  async function one<Schema extends z.ZodTypeAny, Params, Result extends z.infer<Schema>>(
+    schema: Schema,
+    method: PreparedQuery<Params, Result>,
+    params?: Params,
+  ): Promise<z.infer<Schema>> {
+    return query(pool, true, schema, method, params);
+  }
+
+  async function many<
+    Schema extends z.ZodTypeAny,
+    Params extends object,
+    Result extends z.infer<Schema>, // note: the "extends" constraint can be dropped if we just want to rely on the Zod schema, and ignore DB-side types completely
+  >(
+    schema: Schema,
+    method: PreparedQuery<Params, Result>,
+    params: Params,
+  ): Promise<z.infer<Schema>[]>;
+  async function many<
+    Schema extends z.ZodTypeAny,
+    Params extends void,
+    Result extends z.infer<Schema>,
+  >(schema: Schema, method: PreparedQuery<Params, Result>): Promise<z.infer<Schema>[]>;
+  async function many<Schema extends z.ZodTypeAny, Params, Result extends z.infer<Schema>>(
+    schema: Schema,
+    method: PreparedQuery<Params, Result>,
+    params?: Params,
+  ): Promise<z.infer<Schema>[]> {
+    return query(pool, false, schema, method, params);
+  }
 };
 
-async function query<
-  One extends boolean,
-  Schema extends z.ZodTypeAny,
-  Params extends object,
-  Result extends z.infer<Schema>, // note: the "extends" constraint can be dropped if we just want to rely on the Zod schema, and ignore DB-side types completely
->(
-  pool: Pool,
-  one: One,
-  schema: Schema,
-  method: PreparedQuery<Params, Result>,
-  params: Params,
-): Promise<One extends true ? z.infer<Schema> : z.infer<Schema>[]>;
-async function query<
-  One extends boolean,
-  Schema extends z.ZodTypeAny,
-  Params extends void,
-  Result extends z.infer<Schema>,
->(
-  pool: Pool,
-  one: One,
-  schema: Schema,
-  method: PreparedQuery<Params, Result>,
-): Promise<One extends true ? z.infer<Schema> : z.infer<Schema>[]>;
+type GenParams<T> = T extends PreparedQuery<infer P, any> ? P : void;
+
 async function query<
   One extends boolean,
   Schema extends z.ZodTypeAny,
