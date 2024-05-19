0c787ee6cc48a818de07b4b7f2070365b2a09d36
Jarno Rantanen <jarno@jrw.fi>
2024-05-19 16:07:12 +0300

    Add compiled API.

---
+++ api/test.js
@@ -0,0 +1,13 @@
+"use strict";
+// TODO: import type { VercelRequest, VercelResponse } from '@vercel/node';
+module.exports = (req, res) => {
+    const { name = 'World' } = req.query;
+    console.log('Hello from TS', {
+        body: req.body,
+        headers: req.headers,
+        method: req.method,
+        query: req.query,
+        url: req.url,
+    });
+    res.status(200).send(`Hello, ${name}!`);
+};
