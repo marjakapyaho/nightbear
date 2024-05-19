4d0e8c251f49f252c3430dee2a124d55f8c4b8e2
Jarno Rantanen <jarno@jrw.fi>
2024-05-19 19:07:12 +0300

    Try TS instead.

--- src/backend/api/test.ts
+++ src/backend/api/test.ts
@@ -1,22 +1,22 @@
 // TODO: import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-module.exports = (req, res) => {
+module.exports = (req: any, res: any) => {
   const fs = require('fs');
 
   // Get the current working directory
-  const currentDir = process.cwd();
+  const currentDir = process.cwd() + 'src';
 
   // Log the current working directory
   console.log('Current working directory:', currentDir);
 
-  const dirContent = []
+  const dirContent: string[] = []
   try {
       // Read the contents of the current directory
       const files = fs.readdirSync(currentDir);
 
       // Log the contents of the directory
       console.log('Contents of the directory:', currentDir);
-      files.forEach(file => {
+      files.forEach((file: string) => {
           console.log(file);
           dirContent.push(file)
       });
