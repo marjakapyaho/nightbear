1e1f7537c03405587689867f6a4105850e181185
Jarno Rantanen <jarno@jrw.fi>
2024-05-19 19:04:13 +0300

    List files.

--- src/backend/api/test.js
+++ src/backend/api/test.js
@@ -1,13 +1,28 @@
 // TODO: import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 module.exports = (req, res) => {
-  const { name = 'World' } = req.query;
-  console.log('Hello from TS', {
-    body: req.body,
-    headers: req.headers,
-    method: req.method,
-    query: req.query,
-    url: req.url,
-  })
-  res.status(200).send(`Hello, ${name}!`);
+  const fs = require('fs');
+
+  // Get the current working directory
+  const currentDir = process.cwd();
+
+  // Log the current working directory
+  console.log('Current working directory:', currentDir);
+
+  const dirContent = []
+  try {
+      // Read the contents of the current directory
+      const files = fs.readdirSync(currentDir);
+
+      // Log the contents of the directory
+      console.log('Contents of the directory:', currentDir);
+      files.forEach(file => {
+          console.log(file);
+          dirContent.push(file)
+      });
+  } catch (err) {
+      console.error('Unable to scan directory:', err);
+  }
+
+  res.status(200).send(JSON.stringify({currentDir, dirContent}, null, 2));
 };
