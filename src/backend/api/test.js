a7b7fcd274337d6d07c39ecc14fa9c42ca860ab1
Jarno Rantanen <jarno@jrw.fi>
2024-05-19 19:09:28 +0300

    Try combining with build step.

--- src/backend/api/test.js
+++ src/backend/api/test.js
@@ -1,22 +1,22 @@
 // TODO: import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-module.exports = (req: any, res: any) => {
+module.exports = (req, res) => {
   const fs = require('fs');
 
   // Get the current working directory
-  const currentDir = process.cwd() + 'src';
+  const currentDir = process.cwd();
 
   // Log the current working directory
   console.log('Current working directory:', currentDir);
 
-  const dirContent: string[] = []
+  const dirContent = []
   try {
       // Read the contents of the current directory
       const files = fs.readdirSync(currentDir);
 
       // Log the contents of the directory
       console.log('Contents of the directory:', currentDir);
-      files.forEach((file: string) => {
+      files.forEach((file) => {
           console.log(file);
           dirContent.push(file)
       });
