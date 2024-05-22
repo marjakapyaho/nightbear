6bc6fe037b9e20e02845a51e8244643ed6505175
Marja Käpyaho <marja.kapyaho@futurice.com>
2024-05-22 10:40:01 +0300

    removed build

--- backend/build/cronjobs/temp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.temp = void 0;
-const temp = async (context, _journal) => {
-    const { log } = context;
-    log(`Hello from a Cronjob!`);
-};
-exports.temp = temp;
-//# sourceMappingURL=temp.js.map
