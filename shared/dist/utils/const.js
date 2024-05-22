fc06c05bcafb687773d26a33cf367b4216cac664
Marja Käpyaho <marja.kapyaho@futurice.com>
2024-05-22 22:23:14 +0300

    removed dists

--- shared/dist/utils/const.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WATCH_NAME = exports.NO_WAKE_UPS_PROFILE_NAME = exports.NIGHT_PROFILE_NAME = void 0;
-exports.NIGHT_PROFILE_NAME = 'night';
-exports.NO_WAKE_UPS_PROFILE_NAME = 'no-wake-ups';
-exports.WATCH_NAME = 'fitbit';
-//# sourceMappingURL=const.js.map
--- shared/dist/utils/promise.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.reject = void 0;
-// Convenience helper for creating a rejected Promise containing an Error
-function reject(message) {
-    return Promise.reject(new Error(message));
-}
-exports.reject = reject;
-//# sourceMappingURL=promise.js.map
