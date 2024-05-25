5943e0a32f056aacf6de6a6c64c1afaa513c8736
Jarno Rantanen <jarno@jrw.fi>
2024-05-25 22:04:33 +0300

    Log using inspect.

--- backend/api/netlify-api.ts
+++ backend/api/netlify-api.ts
@@ -2,6 +2,6 @@ import type { Context } from '@netlify/functions';
 
 export default async (req: Request, context: Context) => {
   console.log('Hello from Nightbear API!');
-  console.log(JSON.stringify({ req, context }));
+  console.log({ req, context });
   return new Response('Hello, world!');
 };
