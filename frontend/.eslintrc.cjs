a68146a30bffa27b4d9eda866295a91fc4a1ea13
Marja Käpyaho <marja.kapyaho@futurice.com>
2024-05-31 22:48:35 +0300

    fixed eslintrc

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,23 +1,44 @@
-const base = require('../eslintrc.base.js')
-
 module.exports = {
-  ...base,
-  root: true,
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ...base.parserOptions,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     project: __dirname + '/tsconfig.json',
   },
-  env: { browser: true, es2020: true },
-  extends: [...base.extends, 'plugin:react-hooks/recommended', 'plugin:prettier/recommended'],
+  env: {
+    es2021: true,
+    browser: true
+  },
   plugins: ['prettier', 'react-refresh'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended'
+  ],
   rules: {
-    ...base.rules,
+    'prettier/prettier': ['error'],
+    eqeqeq: ['error'],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-console': ['warn'],
+    'no-var': 'error',
     'react-refresh/only-export-components': 'warn',
     '@typescript-eslint/restrict-template-expressions': 'off',
     '@typescript-eslint/unbound-method': 'off',
     'react-hooks/exhaustive-deps': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off'
   },
+  root: true,
+  ignorePatterns: ['.eslintrc.cjs'],
   settings: {
     'react': {
       version: 'detect'
