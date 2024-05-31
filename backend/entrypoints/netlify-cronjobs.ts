d172ae591d145e259055a12b738661966d64ccaa
Jarno Rantanen <jarno@jrw.fi>
2024-05-31 22:23:34 +0300

    Update Netlify cronjobs to actually do something.

--- backend/entrypoints/netlify-cronjobs.ts
+++ backend/entrypoints/netlify-cronjobs.ts
@@ -1,5 +1,15 @@
-export default async (req: Request) => {
-  const { next_run } = await req.json()
+import debug from 'debug'
+import { devDataImport } from '../cronjobs/devDataImport/devDataImport'
+import { createNodeContext } from '../utils/api'
+import { runCronJobs } from '../utils/cronjobs'
+import { consoleLogStream } from '../utils/logging'
 
-  console.log('Received event! Next invocation at:', next_run)
+debug.log = consoleLogStream // direct log output to where we want it
+
+const context = createNodeContext() // create application runtime context
+
+export default async (_req: Request) => {
+  runCronJobs(context, {
+    devDataImport,
+  })
 }
