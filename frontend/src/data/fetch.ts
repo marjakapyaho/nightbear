b6d32d99be824adc361b97d3c4c7fdb407fdff5c
Jarno Rantanen <jarno@jrw.fi>
2024-06-08 21:54:02 +0300

    Update frontend so it can provide API key.

--- frontend/src/data/fetch.ts
+++ frontend/src/data/fetch.ts
@@ -4,15 +4,22 @@ const API_ROOT = window.location.host.startsWith('localhost')
   ? 'http://localhost:4000' // this is where we can reach API in local dev
   : '//api' // ...and here when actually deployed somewhere
 
+if (!window.localStorage[API_KEY_HEADER]) {
+  window.localStorage[API_KEY_HEADER] = prompt('API said "Unauthorized", update API key:')
+}
+
 export const callFetch = async <T>(route: string, method = 'GET', bodyObj?: T) => {
   return fetch(`${API_ROOT}${route}`, {
     method,
     headers: {
-      [API_KEY_HEADER]: 'DEV_API_KEY', // TODO: MAKE DYNAMIC
+      [API_KEY_HEADER]: window.localStorage[API_KEY_HEADER],
       'Content-Type': bodyObj ? 'application/json' : 'text/plain',
     },
     body: JSON.stringify(bodyObj),
   }).then((res: Response) => {
+    if (res.status === 401) {
+      delete window.localStorage[API_KEY_HEADER]
+    }
     if (!res.ok) {
       return Promise.reject(res.json())
     }
