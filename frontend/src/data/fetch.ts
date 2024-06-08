dfe081b7642bb156439849b79bf581051d5d2dd9
Jarno Rantanen <jarno@jrw.fi>
2024-06-08 21:39:02 +0300

    Update frontend so it provides API key when needed.

--- frontend/src/data/fetch.ts
+++ frontend/src/data/fetch.ts
@@ -1,9 +1,16 @@
-const API_ROOT = 'http://localhost:4000'
+import { API_KEY_HEADER } from '@nightbear/shared'
+
+const API_ROOT = window.location.host.startsWith('localhost')
+  ? 'http://localhost:4000' // this is where we can reach API in local dev
+  : '//api' // ...and here when actually deployed somewhere
 
 export const callFetch = async <T>(route: string, method = 'GET', bodyObj?: T) => {
   return fetch(`${API_ROOT}${route}`, {
     method,
-    headers: bodyObj ? { 'Content-Type': 'application/json' } : undefined,
+    headers: {
+      [API_KEY_HEADER]: 'DEV_API_KEY', // TODO: MAKE DYNAMIC
+      'Content-Type': bodyObj ? 'application/json' : 'text/plain',
+    },
     body: JSON.stringify(bodyObj),
   }).then((res: Response) => {
     if (!res.ok) {
