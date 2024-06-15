c2c182438f1ca7324a4ea41b70ab93f148630002
Jarno Rantanen <jarno@jrw.fi>
2024-06-15 16:35:43 +0300

    Fix lint errors from DexcomShareClient.

--- backend/cronjobs/dexcom/dexcomShareClient.ts
+++ backend/cronjobs/dexcom/dexcomShareClient.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios'
-import { Logger, extendLogger } from '../../utils/logging'
 import { pick } from 'lodash'
+import { Logger, extendLogger } from '../../utils/logging'
 
 const DEXCOM_APPLICATION_ID = 'd8665ade-9673-4e27-9ff6-92db4ce13d13'
 
@@ -25,9 +25,9 @@ export function createDexcomShareClient(username: string, password: string, logg
   log('DexcomShareClient initialized')
 
   return {
-    login(): Promise<string> {
-      return axios
-        .post(
+    async login() {
+      try {
+        const res = await axios.post(
           'https://shareous1.dexcom.com/ShareWebServices/Services/General/LoginPublisherAccountByName',
           {
             accountName: username,
@@ -42,27 +42,25 @@ export function createDexcomShareClient(username: string, password: string, logg
             },
           },
         )
-        .then(res => {
-          if (res.data === '00000000-0000-0000-0000-000000000000') {
-            throw new Error(`DexcomShareClient login request failed due to invalid credentials`)
-          } else {
-            return res.data
-          }
-        })
-        .catch((err: AxiosError) => {
-          if (err.isAxiosError)
-            log(
-              'Login request failed:\n%O',
-              pick(err.response, 'data', 'status', 'statusText', 'headers'),
-            )
-          return Promise.reject(
-            new Error(`DexcomShareClient login request failed (caused by\n${err}\n)`),
+
+        if (res.data === '00000000-0000-0000-0000-000000000000') {
+          throw new Error(`DexcomShareClient login request failed due to invalid credentials`)
+        } else {
+          return String(res.data)
+        }
+      } catch (err) {
+        if (err instanceof AxiosError) {
+          log(
+            'Login request failed:\n%O',
+            pick(err.response, 'data', 'status', 'statusText', 'headers'),
           )
-        })
+        }
+        throw new Error(`DexcomShareClient login request failed (caused by\n${String(err)}\n)`)
+      }
     },
-    fetchBg(sessionId: string): Promise<Array<DexcomShareResponse>> {
-      return axios
-        .post(
+    async fetchBg(sessionId: string) {
+      try {
+        const res = await axios.post(
           `https://shareous1.dexcom.com/ShareWebServices/Services/Publisher/ReadPublisherLatestGlucoseValues?sessionId=${sessionId}&minutes=1440&maxCount=1`,
           undefined,
           {
@@ -72,17 +70,16 @@ export function createDexcomShareClient(username: string, password: string, logg
             },
           },
         )
-        .then(res => res.data)
-        .catch((err: AxiosError) => {
-          if (err.isAxiosError)
-            log(
-              'BG fetch request failed:\n%O',
-              pick(err.response, 'data', 'status', 'statusText', 'headers'),
-            )
-          return Promise.reject(
-            new Error(`DexcomShareClient BG fetch request failed (caused by\n${err}\n)`),
+        return res.data as unknown as DexcomShareResponse[]
+      } catch (err) {
+        if (err instanceof AxiosError) {
+          log(
+            'BG fetch request failed:\n%O',
+            pick(err.response, 'data', 'status', 'statusText', 'headers'),
           )
-        })
+        }
+        throw new Error(`DexcomShareClient BG fetch request failed (caused by\n${String(err)}\n)`)
+      }
     },
   }
 }
