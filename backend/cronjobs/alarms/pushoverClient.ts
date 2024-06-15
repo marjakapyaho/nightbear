bd6eb2847a3eecc08f629a581b33ed1380077a06
Jarno Rantanen <jarno@jrw.fi>
2024-06-15 14:25:07 +0300

    Replace pushover-notifications package with just axios.

--- backend/cronjobs/alarms/pushoverClient.ts
+++ backend/cronjobs/alarms/pushoverClient.ts
@@ -1,15 +1,14 @@
-// eslint-disable @typescript-eslint/no-unsafe-assignment
-// eslint-disable @typescript-eslint/no-unsafe-call
-import axios from 'axios'
-import Pushover from 'pushover-notifications'
 import { Situation } from '@nightbear/shared'
+import axios from 'axios'
+import _ from 'lodash'
 import { Logger, extendLogger } from '../../utils/logging'
 
 export type PushoverClient = ReturnType<typeof createPushoverClient>
 
 export const NO_PUSHOVER: PushoverClient = {
-  sendAlarm: async () => undefined,
-  ackAlarms: async () => [],
+  // eslint-disable-next-line @typescript-eslint/require-await
+  sendAlarm: async () => 'NO_PUSHOVER: Mock receipt',
+  ackAlarms: async () => {},
 }
 
 export const createPushoverClient = (
@@ -18,48 +17,40 @@ export const createPushoverClient = (
   callbackUrl: string,
   logger: Logger,
 ) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  const api = new Pushover({ user, token })
   const log = extendLogger(logger, 'pushover')
 
   return {
     async sendAlarm(situation: Situation, recipient: string) {
-      const message = {
-        title: 'Nightbear alarm',
-        sound: 'persistent',
-        message: situation,
-        device: recipient,
-        priority: 2,
-        retry: 30,
-        expire: 10800,
-        callback: `${callbackUrl}?ackedBy=pushover:${recipient}`,
+      try {
+        const res = await axios.post('https://api.pushover.net/1/messages.json', {
+          user,
+          token,
+          title: 'Nightbear alarm',
+          sound: 'persistent',
+          message: situation,
+          device: recipient,
+          priority: 2,
+          retry: 30,
+          expire: 10800,
+          callback: `${callbackUrl}?ackedBy=pushover:${recipient}`,
+        })
+        const receipt = String(_.get(res, 'data.receipt'))
+        log('Alarm sent with receipt:', receipt)
+        return receipt
+      } catch (err) {
+        log('Could not send alarm:', err)
+        throw err
       }
-
-      return new Promise((resolve, reject) =>
-        api.send(message, (err: object, result: string) => {
-          if (err) {
-            log('Could not send alarm:', err)
-            return reject(err)
-          }
-
-          const receipt: string = JSON.parse(result).receipt
-          log('Alarm sent with receipt:', receipt)
-
-          return resolve(receipt)
-        }),
-      )
     },
 
     async ackAlarms(receipts: string[] = []) {
-      return Promise.all(
-        receipts.map(receipt => {
-          log('Acking alarm with receipt:', receipt)
-
-          return axios.post(
-            'https://api.pushover.net/1/receipts/' + receipt + '/cancel.json',
-            'token=' + encodeURIComponent(token),
-            { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
-          )
+      log('Acking alarms with receipts:', receipts)
+      await Promise.all(
+        receipts.map(async receipt => {
+          await axios.post('https://api.pushover.net/1/receipts/' + receipt + '/cancel.json', {
+            token,
+          })
+          log('Acked alarm with receipt:', receipt)
         }),
       )
     },
