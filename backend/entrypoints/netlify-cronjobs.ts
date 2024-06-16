d3f2f89345f94274f24d2c137600260ed59028bb
Jarno Rantanen <jarno@jrw.fi>
2024-06-16 15:15:37 +0300

    Deploy test A.

--- backend/entrypoints/netlify-cronjobs.ts
+++ backend/entrypoints/netlify-cronjobs.ts
@@ -9,5 +9,6 @@ debug.log = consoleLogStream // direct log output to where we want it
 const context = createNodeContext() // create application runtime context
 
 export default async (_req: Request) => {
+  console.log('Deploy test A')
   await runCronJobs(context, cronjobs)
 }
